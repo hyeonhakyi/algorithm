8c1fc94dc6621b880c8402e77ac82500701a975e
코테 <108259054+hyeonhakyi@users.noreply.github.com>
2024-01-17 22:58:49 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Solution {
+    public int solution(String myString, String pat) {
+        myString = myString.replace("A", "a").replace("B", "A").replace("a", "B");
+        return myString.contains(pat) ? 1 : 0;
+    }
+}

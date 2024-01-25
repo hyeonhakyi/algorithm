3f4bd027425e37389050ed5f9338da4e62702672
코테 <108259054+hyeonhakyi@users.noreply.github.com>
2024-01-25 21:16:31 +0900

    [Silver IV] Title: 수 찾기, Time: 1364 ms, Memory: 183964 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i <n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int m = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i <m; i++){
+            if(binarySearch(arr, sc.nextInt()) >=0 ){
+                sb.append(1).append('\n');
+            }
+            else{
+                sb.append(0).append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+    public static int binarySearch (int[] arr, int key){
+        int lo = 0;
+        int hi = arr.length - 1;
+
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+
+            if (key < arr[mid]) {
+                hi = mid - 1;
+            } else if (key > arr[mid]) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}

5ec0a7d1eca015412f8d1fda93e975cd937893a0
코테 <108259054+hyeonhakyi@users.noreply.github.com>
2024-02-05 23:36:39 +0900

    [Gold V] Title: 탑, Time: 676 ms, Memory: 168784 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+class Top{
+    int num;
+    int height;
+
+    Top(int num, int height){
+        this.height = height;
+        this.num = num;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+
+        Stack<Top> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= n; i++){
+            int height = Integer.parseInt(st.nextToken());
+
+            if(stack.isEmpty()){
+                sb.append("0 ");
+                stack.push(new Top(i,height));
+            }else {
+                while (true) {
+                    if (stack.isEmpty()) {
+                        sb.append("0 ");
+                        stack.push(new Top(i, height));
+                        break;
+                    }
+
+                    Top top = stack.peek();
+
+                    if(top.height > height){
+                        sb.append(top.num+" ");
+                        stack.push(new Top(i,height));
+                        break;
+                    }else{
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        System.out.println(sb.toString() + "\n");
+    }
+}

c437dcc462bd4a67dd99d8bc33a7711f6b73c387
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2024-03-10 00:31:23 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.06 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -4,69 +4,70 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Solution {
-    private static int[] percent = {0,10,20,30,40};
-    private static int total_join = 0 , total_price = 0 , min = Integer.MAX_VALUE;
-    public int[] solution(int[][] users,int[] emoticons) {
+     private static int[] percent = {0,10,20,30,40};
+    private static int total_count = 0 , total_price = 0, min = Integer.MAX_VALUE;
+    public int[] solution(int[][] users,int[] emoticons){
         for(int[] user : users){
-            min = Math.min(min, user[0]);
+            min = Math.min(min,user[0]);
         }
+
         for(int i = 0; i < 5; i++){
             if(min <= percent[i]){
                 min = i;
                 break;
             }
         }
-        int[] discounts = new int[emoticons.length];
-        comb(discounts, 0, emoticons.length, users, emoticons);
 
-        int[] answer = {total_join, total_price};
-        return answer;
-    }//main end
+        int[] discount = new int[emoticons.length];
+        dfs(discount,0,emoticons.length,users,emoticons);
+        int[] result = {total_count,total_price};
+        return result;
+    }//solution end
 
-    private void comb(int[] discounts,int s,int n,int[][] users,int[] emoticons){
+    private static void dfs(int[] discount,int s,int n,int[][] users,int[] emoticons){
         if(s==n){
-            cal(users,emoticons,discounts);
+            check(discount,users,emoticons);
             return;
         }
 
         for(int i = s; i < n; i++){
-            for(int j = min; j < 5; j++){
-                discounts[i] = percent[j];
-                comb(discounts, i + 1, n , users, emoticons);
+            for(int j = min; j< 5; j++){
+                discount[i] = percent[j];
+                dfs(discount,i+1,n,users,emoticons);
             }
         }
-    }//comb end
+    }//dfs end
 
-    private void cal(int[][] users, int[] emoticons,int[] discounts){
-        int join = 0;
+    private static void check(int[] discount,int[][] users,int[] emoticons){
+        int count = 0;
         int price = 0;
 
         for(int[] user : users){
             int userMinDiscount = user[0];
-            int userMaxPay = user[1];
+            int userPrice = user[1];
             int sum = 0;
 
-            for(int i = 0; i < discounts.length; i++){
-                if(discounts[i] < userMinDiscount)continue;
-                sum += sale(emoticons[i], discounts[i]);
+            for(int i = 0; i < discount.length; i++){
+                if(discount[i] < userMinDiscount)continue;
+                sum += cal(discount[i],emoticons[i]);
             }
 
-            if(userMaxPay <= sum){
-                join++;
+            if(sum >= userPrice){
+                count++;
             }else{
                 price += sum;
             }
         }
 
-        if(join > total_join){
-            total_join = join;
+        if(total_count < count){
+            total_count = count;
             total_price = price;
-        }else if(join == total_join && price > total_price){
+        }else if(total_count == count && price > total_price){
             total_price = price;
         }
-    }//cal end
+    }//check end
 
-    private static int sale(int price,int percent){
-        return (price/100) * (100 - percent);
+    private static int cal(int dis,int price){
+        return (price/100) * (100 - dis);
     }
 }//class end

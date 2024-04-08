e4427fcbd3aa2adce3b4285b9626fc3ec7eb0289
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2024-04-08 21:01:18 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 108 ms, Memory: 14480 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -5,46 +5,50 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
     private static class Node{
         int x;
         int y;
-        int value;
+        int count;
         int key;
-        public Node(int x,int y,int value,int key){
+        public Node(int x,int y,int count,int key){
             this.x = x;
             this.y = y;
-            this.value = value;
+            this.count = count;
             this.key = key;
         }
     }
-    private static int n,m;
+    private static int n,m,result;
     private static char[][] arr;
+    private static boolean[][][] visited;
     private static int[] dx = {-1,1,0,0};
     private static int[] dy = {0,0,-1,1};
-    private static boolean[][][] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+
         arr = new char[n][m];
         visited = new boolean[n][m][64];
 
         int startX = 0;
         int startY = 0;
+
         for(int i = 0; i < n; i++){
             String s = br.readLine();
             for(int j = 0; j < m; j++){
                 arr[i][j] = s.charAt(j);
 
                 if(arr[i][j] == '0'){
-                    startX = i;
-                    startY = j;
+                   startX = i;
+                   startY = j;
                 }
             }
         }
+
         bfs(startX,startY);
+
     }//main end
 
     private static void bfs(int x,int y){
@@ -54,32 +58,28 @@ public class Main {
 
         while(!que.isEmpty()){
             Node now = que.poll();
-            int nowX = now.x;
-            int nowY = now.y;
-            int value = now.value;
 
-            if(arr[nowX][nowY] == '1'){
-                System.out.println(value);
+            if(arr[now.x][now.y] == '1'){
+                System.out.println(now.count);
                 return;
             }
-
             for(int d = 0; d < 4; d++){
-                int nx = nowX + dx[d];
-                int ny = nowY + dy[d];
+                int nx = now.x + dx[d];
+                int ny = now.y + dy[d];
 
-                if(nx >= 0 && ny >= 0 && nx < n && ny < m && arr[nx][ny] != '#' && !visited[nx][ny][now.key]){
+                if(check(nx,ny) && arr[nx][ny] != '#' && !visited[nx][ny][now.key]){
                     if(arr[nx][ny] >= 'a' && arr[nx][ny] <= 'z'){
-                        int key = now.key | (1 << (arr[nx][ny] - 'a'));
-                        que.offer(new Node(nx,ny,value+1,key));
+                        int key = now.key | 1 << (arr[nx][ny] - 'a');
+                        que.offer(new Node(nx,ny, now.count+1,key));
                         visited[nx][ny][key] = true;
                     }else if(arr[nx][ny] >= 'A' && arr[nx][ny] <= 'Z'){
-                        boolean check = (now.key & (1 << (arr[nx][ny] - 'A')))!= 0;
-                        if(check){
-                            que.offer(new Node(nx,ny,value+1, now.key));
+                        boolean con = (now.key & 1 << (arr[nx][ny] - 'A'))!= 0;
+                        if(con){
+                            que.offer(new Node(nx,ny, now.count+1,now.key));
                             visited[nx][ny][now.key] = true;
                         }
                     }else{
-                        que.offer(new Node(nx,ny,value+1,now.key));
+                        que.offer(new Node(nx,ny, now.count+1, now.key));
                         visited[nx][ny][now.key] = true;
                     }
                 }
@@ -87,4 +87,8 @@ public class Main {
         }
         System.out.println(-1);
     }//bfs end
+
+    private static boolean check(int x,int y){
+        return x >= 0 && y >= 0 && x < n && y < m;
+    }
 }//class end

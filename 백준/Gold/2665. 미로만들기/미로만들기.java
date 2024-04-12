f237ce01b49d489311415418d69f699aade39cc2
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2024-04-12 20:32:35 +0900

    [Gold IV] Title: 미로만들기, Time: 88 ms, Memory: 11784 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.java
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -1,23 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.Queue;
 
 public class Main {
-    private static class Node implements Comparable<Node>{
+    private static class Node implements Comparable<Node> {
         int x;
         int y;
         int count;
-        public Node(int x,int y,int count){
+        int pass;
+        public Node(int x,int y,int count,int pass){
             this.x = x;
             this.y = y;
             this.count = count;
+            this.pass = pass;
         }
 
         @Override
         public int compareTo(Node o) {
-            return Integer.compare(this.count,o.count);
+            return Integer.compare(this.pass,o.pass);
         }
     }
     private static int n,result;
@@ -34,39 +38,38 @@ public class Main {
         for(int i = 0; i < n; i++){
             String s = br.readLine();
             for(int j = 0; j < n; j++){
-                arr[i][j] = s.charAt(j) - '0';
+                arr[i][j] = s.charAt(j) -'0';
             }
         }
-
         bfs();
         System.out.println(result);
-    }//main class
+    }//main end
 
     private static void bfs(){
         PriorityQueue<Node> que = new PriorityQueue<>();
-        que.offer(new Node(0,0,0));
+        que.offer(new Node(0,0,0,0));
         visited[0][0] = true;
 
         while(!que.isEmpty()){
             Node now = que.poll();
 
             if(now.x == n-1 && now.y == n-1){
-                result = now.count;
+                result = now.pass;
                 return;
             }
+
 
             for(int d = 0; d < 4; d++){
                 int nx = now.x + dx[d];
                 int ny = now.y + dy[d];
 
-                if(check(nx,ny) && !visited[nx][ny]){
-                    if(arr[nx][ny] == 1){
-                        visited[nx][ny] = true;
-                        que.offer(new Node(nx,ny,now.count));
-                    }else if(arr[nx][ny] == 0){
-                        visited[nx][ny] = true;
-                        que.offer(new Node(nx,ny,now.count+1));
-                    }
+                if(!check(nx,ny))continue;
+                if(visited[nx][ny])continue;
+                visited[nx][ny] = true;
+                if(arr[nx][ny] == 0){
+                    que.offer(new Node(nx,ny,now.count+1,now.pass+1));
+                }else{
+                    que.offer(new Node(nx,ny,now.count+1,now.pass));
                 }
             }
         }
@@ -74,5 +77,5 @@ public class Main {
 
     private static boolean check(int x,int y){
         return x >= 0 && y >= 0 && x < n && y < n;
-    }//check end
+    }
 }//class end

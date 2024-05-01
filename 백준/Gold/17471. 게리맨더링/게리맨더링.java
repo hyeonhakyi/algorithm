6f1a725f02d165984cdd5699d60930b6088f82b9
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2024-05-01 16:47:29 +0900

    [Gold III] Title: 게리맨더링, Time: 112 ms, Memory: 13388 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -7,110 +7,109 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int n,min = Integer.MAX_VALUE;
-    private static int[] arr;
-    private static boolean[] visited;
-    private static boolean[] check;
-    private static ArrayList<Integer>[] list;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        n = Integer.parseInt(br.readLine());
-
-        arr = new int[n+1];
-        visited = new boolean[n+1];
-        list = new ArrayList[n+1];
-
-        for(int i = 1; i <= n; i++){
-            list[i] = new ArrayList<>();
-        }
-
-        st = new StringTokenizer(br.readLine());
-        for(int i = 1; i <= n; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        for(int i = 1; i <= n; i++){
-            st = new StringTokenizer(br.readLine());
-            int num = Integer.parseInt(st.nextToken());
-            for(int j = 0; j < num; j++){
-                list[i].add(Integer.parseInt(st.nextToken()));
-            }
-        }
-
-        dfs(0);
-        if(min == Integer.MAX_VALUE){
-            System.out.println(-1);
-        }else{
-            System.out.println(min);
-        }
-    }//main end
-
-    private static void dfs(int count){
-        if(count == n){
-            ArrayList<Integer> trueList = new ArrayList<>();
-            ArrayList<Integer> falseList = new ArrayList<>();
-
-            for(int i = 1; i <= n; i++){
-                if(visited[i]){
-                    trueList.add(i);
-                }else{
-                    falseList.add(i);
-                }
-            }
-
-            if(trueList.isEmpty() || falseList.isEmpty()){
-                return;
-            }
-
-            if(bfs(trueList) && bfs(falseList)){
-                diff();;
-            }
-            return;
-        }
-
-        visited[count] = true;
-        dfs(count+1);
-        visited[count] = false;
-        dfs(count+1);
-    }//dfs end
-
-    private static boolean bfs(ArrayList<Integer> arr){
-        Queue<Integer> que = new LinkedList<>();
-        check = new boolean[n+1];
-        que.offer(arr.get(0));
-        check[arr.get(0)] = true;
-        
-        int count = 1;
-        while (!que.isEmpty()){
-            int now = que.poll();
-            
-            for(int i = 0; i < list[now].size(); i++){
-                int next = list[now].get(i);
-                if(arr.contains(next) && !check[next]){
-                    que.offer(next);
-                    check[next] = true;
-                    count++;
-                }
-            }
-        }
-        if(count == arr.size()){
-            return true;
-        }else{
-            return false;
-        }
-    }//bfs end
-    
-    private static void diff(){
-        int trueNum = 0;
-        int falseNum = 0;
-        for(int i = 1; i<= n; i++){
-            if(check[i]){
-                trueNum += arr[i];
-            }else{
-                falseNum += arr[i];
-            }
-        }
-        min = Math.min(min,Math.abs(trueNum-falseNum));
-    }//dif end
+	private static int n,result = Integer.MAX_VALUE;
+	private static int[] arr;
+	private static boolean[] visited;
+	private static boolean[] check;
+	private static ArrayList<Integer>[] list;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n+1];
+		visited = new boolean[n+1];
+		list = new ArrayList[n+1];
+		
+		for(int i = 1; i <= n; i++) {
+			list[i] = new ArrayList<>();
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int num = Integer.parseInt(st.nextToken());
+			for(int j = 0; j < num; j++) {
+				list[i].add(Integer.parseInt(st.nextToken()));
+			}
+		}
+		
+		dfs(0);
+		if(result == Integer.MAX_VALUE) {
+			System.out.println(-1);
+		}else {
+			System.out.println(result);
+		}
+	}//main end
+	
+	private static void dfs(int count) {
+		if(count == n) {
+			ArrayList<Integer> truelist = new ArrayList<>();
+			ArrayList<Integer> falseList = new ArrayList<>();
+			
+			for(int i = 1; i <= n; i++) {
+				if(visited[i]) {
+					truelist.add(i);
+				}else {
+					falseList.add(i);
+				}
+			}
+			if(truelist.isEmpty() || falseList.isEmpty()) {
+				return;
+			}
+			
+			if(bfs(truelist) && bfs(falseList)) {
+				diff();
+			}
+			return;
+		}
+		
+		visited[count] = true;
+		dfs(count + 1);
+		visited[count] = false; 
+		dfs(count + 1);
+	}//dfs end
+	
+	private static boolean bfs(ArrayList<Integer> arr) {
+		Queue<Integer> que = new LinkedList<>();
+		check = new boolean[n+1];
+		que.offer(arr.get(0));
+		check[arr.get(0)] = true;
+		
+		int cnt = 1;
+		while(!que.isEmpty()) {
+			int now = que.poll();
+			
+			for(int i = 0; i < list[now].size(); i++) {
+				int next = list[now].get(i);
+				if(arr.contains(next) && !check[next]) {
+					que.offer(next);
+					check[next] = true;
+					cnt++;
+				}
+			}
+		}
+		if(cnt == arr.size()) {
+			return true;
+		}else {
+			return false;
+		}
+	}//bfs end
+	
+	private static void diff() {
+		int trueSum = 0;
+		int falseSum = 0;
+		
+		for(int i = 1; i <= n; i++) {
+			if(check[i]) {
+				trueSum += arr[i];
+			}else{
+				falseSum += arr[i];
+			}
+		}
+		result = Math.min(result,Math.abs(falseSum - trueSum));
+	}//diff end
 }//class end

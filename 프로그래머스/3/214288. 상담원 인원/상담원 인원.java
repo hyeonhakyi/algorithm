d28dc4ae9d2cdf9bc51a443f817f10059b602224
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2024-06-11 18:15:34 +0900

    [level 3] Title: 상담원 인원, Time: 3.64 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/214288. 상담원 인원/상담원 인원.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class Solution {
+    public static class Node{
+        int start;
+        int time;
+        public Node(int start,int time){
+            this.start = start;
+            this.time = time;
+        }
+    }
+    public int solution(int k, int n, int[][] reqs) {
+        int answer = 0;
+        ArrayList<ArrayList<Node>> list = new ArrayList<>();
+        for(int i = 0; i <= n; i++){
+            list.add(new ArrayList<>());
+        }
+        
+        for(int[] i : reqs){
+            list.get(i[2]).add(new Node(i[0],i[1]));
+        }
+        
+        int[][] total = new int[k+1][n-k+2];
+        for(int i = 1; i <= k; i++){
+            for(int j = 1; j <= n - k + 1; j++){
+                PriorityQueue<Integer> que = new PriorityQueue<>();
+                for(Node nk : list.get(i)){
+                    if(que.size() < j){
+                        que.offer(nk.start + nk.time);
+                    }else{
+                        int min = que.poll();
+                        int wait = min - nk.start;
+                        if(wait > 0){
+                            total[i][j] += wait;
+                            que.offer(min + nk.time);
+                        }else{
+                            que.offer(nk.start + nk.time);
+                        }
+                    }
+                }
+            }
+        }
+        answer = dfs(n-k+1, total, 1, k);
+        return answer;
+    }//solution end
+    
+    public static int dfs(int remain, int[][] wait, int depth, int k){
+        if(depth > k){
+            return 0;
+        }
+        int sum = 100000000;
+        for(int i = 1; i <= remain; i++){
+            sum = Math.min(dfs(remain-i+1, wait, depth+1, k) + wait[depth][i],sum);
+        }
+        return sum;
+    }//dfs end
+}

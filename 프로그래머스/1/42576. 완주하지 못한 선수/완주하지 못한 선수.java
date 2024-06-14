ea89d23f5d479fd1b43ca0a3fe515bd7006ee7bd
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2024-06-14 14:32:11 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.95 ms, Memory: 96.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> set = new HashMap<>();
+        String answer = "";
+        
+        for(int i = 0; i < participant.length; i++){
+            if(set.get(participant[i]) != null){
+                set.put(participant[i],set.get(participant[i]) + 1);
+            }else{
+                set.put(participant[i], 1);
+            }
+        }
+        
+        for(int i = 0; i < completion.length; i++){
+            if(set.get(completion[i]) != null){
+                set.put(completion[i], set.get(completion[i])-1);
+            }
+        }
+        
+        for(String key : set.keySet()){
+            if(set.get(key)!=0){
+                answer = key;
+            }
+        }
+        
+        return answer;
+    }
+}

89c0b0ceb49ea04e52acca1bb7454fe790dd53fa
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2024-06-14 11:28:23 +0900

    [Gold IV] Title: 직사각형 탈출, Time: 3640 ms, Memory: 118560 KB -BaekjoonHub

--- 백준/Gold/16973. 직사각형 탈출/직사각형 탈출.java
+++ 백준/Gold/16973. 직사각형 탈출/직사각형 탈출.java
@@ -1,95 +1,97 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static class Node {
+    private static class Node{
         int x;
         int y;
         int count;
-
-        public Node(int x, int y, int count) {
+        public Node(int x, int y, int count){
             this.x = x;
             this.y = y;
             this.count = count;
         }
     }
-
-    private static int n, m, result, h, w, sx, sy, ex, ey;
-    private static int[][] arr;
-    private static int[] dx = {-1, 1, 0, 0};
-    private static int[] dy = {0, 0, -1, 1};
-
+    private static int n,m;
+    private static int[] dx = {-1,1,0,0};
+    private static int[] dy = {0,0,-1,1};
+    private static int h,w,stX,stY,endX,endY;
+    private static int[][] map;
+    private static int min = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        arr = new int[n + 1][m + 1];
+        map = new int[n][m];
 
-        for (int i = 1; i <= n; i++) {
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= m; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            for(int j = 0; j < m; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         st = new StringTokenizer(br.readLine());
         h = Integer.parseInt(st.nextToken());
         w = Integer.parseInt(st.nextToken());
-        sx = Integer.parseInt(st.nextToken());
-        sy = Integer.parseInt(st.nextToken());
-        ex = Integer.parseInt(st.nextToken());
-        ey = Integer.parseInt(st.nextToken());
+        stX = Integer.parseInt(st.nextToken());
+        stY = Integer.parseInt(st.nextToken());
+        endX = Integer.parseInt(st.nextToken());
+        endY = Integer.parseInt(st.nextToken());
 
-        result = -1;
         bfs();
-        System.out.println(result);
+        if(min == Integer.MAX_VALUE){
+            System.out.println(-1);
+        }else{
+            System.out.println(min);
+        }
     }//main end
 
     private static void bfs() {
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(sx, sy, 0));
-        boolean[][] visited = new boolean[n + 1][m + 1];
-        visited[sx][sy] = true;
+        Queue<Node> que = new LinkedList<>();
+        boolean[][] visited = new boolean[n][m];
+        que.offer(new Node(stX-1,stY-1,0));
+        visited[stX-1][stY-1] = true;
 
-        while (!q.isEmpty()) {
-            Node now = q.poll();
+        while(!que.isEmpty()){
+            Node now = que.poll();
 
-            if (now.x == ex && now.y == ey) {
-                result = now.count;
+            if(now.x == endX-1 && now.y == endY-1){
+                min = now.count;
                 return;
             }
 
-            for (int d = 0; d < 4; d++) {
+            for(int d = 0; d < 4; d++){
                 int nx = now.x + dx[d];
                 int ny = now.y + dy[d];
 
-                if (!check(nx, ny)) continue;
-                if (visited[nx][ny]) continue;
-                if (!innerCheck(nx, ny)) continue;
+                if(!check(nx,ny))continue;
+                if(visited[nx][ny])continue;
+                if(!isValid(nx,ny))continue;
 
-                q.offer(new Node(nx, ny, now.count + 1));
                 visited[nx][ny] = true;
+                que.offer(new Node(nx,ny,now.count+1));
             }
         }
     }//bfs end
 
-    public static boolean innerCheck(int x, int y) {
-        for (int i = x; i < x + h; i++) {
-            for (int j = y; j < y + w; j++) {
-                if (i < 1 || j < 1 || i > n || j > m || arr[i][j] == 1)
+    private static boolean isValid(int x, int y){
+        for(int i = x; i < x + h; i++){
+            for(int j = y; j < y + w; j++){
+                if(i >= n || j >= m || i < 0 || j < 0 || map[i][j] == 1){
                     return false;
+                }
             }
         }
         return true;
-    }
+    }//isValid end
 
-    private static boolean check(int x, int y) {
-        return x >= 1 && x <= n && y >= 1 && y <= m;
-    }
+    private static boolean check(int x,int y){
+        return x >= 0 && x < n && y >= 0 && y < m;
+    }//check end
 }//class end

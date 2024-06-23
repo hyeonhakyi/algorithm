5297f98af90aceeda2328f410ade05a8fa60d86c
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2024-06-23 20:55:02 +0900

    [level 2] Title: 올바른 괄호, Time: 17.44 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        
+        for(char item : s.toCharArray()){
+            if(item == '('){
+                stack.push('(');
+            }else{
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+                    
+        if(stack.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}

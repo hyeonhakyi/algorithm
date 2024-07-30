229ca45d75c7f57707e1ecfb9bce5fb1fd68eb9e
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2024-07-31 01:23:44 +0900

    [Gold V] Title: 나무 위의 빗물, Time: 736 ms, Memory: 190904 KB -BaekjoonHub

---
+++ 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.java
@@ -0,0 +1,36 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static ArrayList<Integer>[] list;
+    static int leaf = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        list = new ArrayList[n + 1];
+        
+        for (int i = 0; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
+        
+        for (int i = 0; i < n - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            list[s].add(e);
+            list[e].add(s);
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (list[i].size() == 1) leaf++;
+        }
+        System.out.println((double) w / leaf);
+    }
+}

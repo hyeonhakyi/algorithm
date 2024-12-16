f7af8f4c0e22228b4e5bce8d03d881ce086fce33
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2024-12-16 15:19:58 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,6 +1,6 @@
-select i.ANIMAL_ID,i.NAME
+SELECT i.ANIMAL_ID, i.NAME
 from ANIMAL_INS as i
 join ANIMAL_OUTS as o
 on i.ANIMAL_ID = o.ANIMAL_ID
 where o.DATETIME <= i.DATETIME
-order by i.DATETIME;
+order by i.DATETIME

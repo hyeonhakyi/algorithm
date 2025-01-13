35854cbf6df9cb0cc762edc805880f1d268d0293
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-01-13 16:36:39 +0900

    [level 3] Title: 부서별 평균 연봉 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
+++ 프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
@@ -1,5 +1,6 @@
-select d.DEPT_ID,d.DEPT_NAME_EN,round(avg(e.SAL),0) as AVG_SAL
-from HR_DEPARTMENT d join HR_EMPLOYEES e
+select d.DEPT_ID, d.DEPT_NAME_EN, round(avg(SAL),0) as AVG_SAL
+from HR_DEPARTMENT as d
+join HR_EMPLOYEES as e
 on d.DEPT_ID = e.DEPT_ID
 group by d.DEPT_ID
-order by AVG_SAL desc;
+order by AVG_SAL desc

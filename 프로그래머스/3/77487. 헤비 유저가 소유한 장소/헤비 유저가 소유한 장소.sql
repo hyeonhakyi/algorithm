41331906d4b90f11ab8b84531560b0d5d0931c68
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-02-03 14:32:31 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,8 +1,7 @@
-SELECT *
+SELECT ID, NAME, HOST_ID
 from PLACES
-where HOST_ID in (
-select HOST_ID
-from PLACES
-group by HOST_ID
-having count(*) >= 2)
-order by ID;
+where HOST_ID in (select HOST_ID
+                 from PLACES
+                 group by HOST_ID
+                 having count(HOST_ID) >= 2)
+order by ID 

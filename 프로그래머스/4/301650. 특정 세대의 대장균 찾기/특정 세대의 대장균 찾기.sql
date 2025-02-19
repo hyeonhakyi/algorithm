7044d0dd5299aed9f0c775ba0d191c57ed7592e7
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-02-19 14:44:47 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,8 +1,6 @@
-select ID
-from ECOLI_DATA 
-where PARENT_ID in (select ID
-                   from ECOLI_DATA
-                   where PARENT_ID in (select ID
-                                      from ECOLI_DATA
-                                      where PARENT_ID is null))
-order by ID;
+select f.ID
+from ECOLI_DATA as f
+join ECOLI_DATA as s on s.ID = f.PARENT_ID
+join ECOLI_DATA as t on t.ID = s.PARENT_ID
+where t.PARENT_ID is null
+order by f.ID;

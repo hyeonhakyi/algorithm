d376fc081e4b5407f6adc30f557e674c24ab0fdd
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-02-27 15:13:25 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.05 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, long k) {
+        int[] answer = new int[n];
+        List<Integer> list = new ArrayList<>();
+        long total = 1;
+        
+        for(int i = 1; i <= n; i++){
+            list.add(i);
+            total *= i;
+        }
+        
+        k--;
+        int idx = 0;
+        while(idx < n){
+            total /= n - idx;
+            answer[idx++] = list.remove((int) (k/total));
+            k %= total;
+        }
+        return answer;
+    }
+}

fe5ff4ecdc7f117042bd9178565ae9b973e7e09f
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-03-25 13:31:29 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,5 +1,5 @@
-select hour(DATETIME) as hour, count(*) as count
-from ANIMAL_OUTS
-group by hour
-having hour between 9 and 20
-order by hour;
+select hour(DATETIME) as HOUR, count(*) as COUNT
+from ANIMAL_OUTS 
+where hour(DATETIME) between 9 and 19
+group by HOUR
+order by HOUR

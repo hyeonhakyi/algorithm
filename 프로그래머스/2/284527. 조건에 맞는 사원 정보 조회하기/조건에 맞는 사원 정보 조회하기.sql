53044c9a6c170507b85413af1fd0c6fdb3444140
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-03-28 13:00:12 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,7 +1,8 @@
-select sum(g.score) as SCORE,e.EMP_NO, e.EMP_NAME, e.POSITION,e.EMAIL
-from HR_GRADE g join HR_EMPLOYEES e
-on e.EMP_NO = g.EMP_NO
-group by YEAR,EMP_NO
-having g.YEAR = '2022'
-order by 1 desc
-limit 1;
+select sum(hg.SCORE) as SCORE , he.EMP_NO, he.EMP_NAME, he.POSITION, he.EMAIL
+from HR_EMPLOYEES  as he
+join HR_DEPARTMENT as hd on he.DEPT_ID = hd.DEPT_ID
+join HR_GRADE as hg on he.EMP_NO = hg.EMP_NO
+group by YEAR,hg.EMP_NO
+having hg.YEAR like '2022'
+order by SCORE desc
+limit 1 

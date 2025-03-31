b5d7cec1f199b5171b23d2c90645f622917ad333
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-03-31 14:48:21 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,5 +1,5 @@
-select ID,EMAIL,FIRST_NAME,LAST_NAME
-from DEVELOPERS 
-where SKILL_CODE & (select CODE from SKILLCODES where name = 'Python')
-or SKILL_CODE & (select CODE from SKILLCODES where name = 'C#')
-order by ID;
+select ID, EMAIL, FIRST_NAME, LAST_NAME
+from DEVELOPERS
+where SKILL_CODE & (select CODE from SKILLCODES where NAME like 'Python')
+or SKILL_CODE & (select CODE from SKILLCODES where NAME like 'C#')
+order by ID

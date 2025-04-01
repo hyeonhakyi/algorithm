3722bcbb0cd15a7cc7062546037734f1a53fac89
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-04-01 17:39:28 +0900

    [Silver II] Title: 창고 다각형, Time: 76 ms, Memory: 11972 KB -BaekjoonHub

---
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[] height = new int[1002];
+        int answer = 0;
+
+        int start = Integer.MAX_VALUE;
+        int end = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int l = Integer.parseInt(st.nextToken());
+            int h = Integer.parseInt(st.nextToken());
+
+            height[l] = h;
+            start = Math.min(start, l);
+            end = Math.max(end, l);
+        }
+
+        int maxIdx = 0;
+        int maxHeight = 0;
+
+        for(int i = start; i <= end; i++){
+            if(height[i] > maxHeight){
+                maxHeight = height[i];
+                maxIdx = i;
+            }
+        }
+
+        int current = 0;
+        for(int i = start; i <= maxIdx; i++){
+            current = Math.max(height[i], current);
+            answer += current;
+        }
+
+        current = 0;
+        for(int i = end; i > maxIdx; i--){
+            current = Math.max(height[i], current);
+            answer += current;
+        }
+
+        System.out.println(answer);
+
+    }//main end
+}//class end

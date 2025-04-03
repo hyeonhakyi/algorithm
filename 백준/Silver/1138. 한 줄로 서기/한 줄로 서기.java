424a285d473d83958b1ca37a46ae4d3bc939252a
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-04-03 18:19:07 +0900

    [Silver II] Title: 한 줄로 서기, Time: 68 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for(int i = n; i >= 1; i--) {
+            list.add(arr[i],i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i : list){
+            sb.append(i).append(" ");
+        }
+
+        System.out.println(sb);
+    }//main end
+}

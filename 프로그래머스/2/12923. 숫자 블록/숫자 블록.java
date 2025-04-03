212eb9a7c23a87b8a3736014229334037ccbdb6b
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-04-03 16:22:00 +0900

    [level 2] Title: 숫자 블록, Time: 328.34 ms, Memory: 54.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(long begin, long end) {
+        int[] answer = new int[(int)(end - begin) + 1];
+        
+        for(long i = begin; i <= end; i++){
+            answer[(int)(i - begin)] = (int)cal(i);
+        }
+        
+        return answer;
+    }
+    
+    public static long cal(long num){
+        int result = 1;
+        
+        if(num == 1) return 0;
+        
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            if(num % i == 0){
+                if(num / i <= 10000000){
+                    result = Math.max(result, (int)(num/i));
+                    break;
+                }else{
+                    result = Math.max(result,i);
+                }
+            }
+        }
+        return result;
+    }
+}

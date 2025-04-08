b4443ca161573142d5d9cd06d850f20f0fcaee12
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-04-08 16:32:37 +0900

    [Silver V] Title: 덩치, Time: 72 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,25 +1,31 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        int n = sc.nextInt();
-        int[][] arr = new int[n][2];
-        for(int i = 0; i< n; i++){
-            arr[i][0] = sc.nextInt();
-            arr[i][1] = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int weight = Integer.parseInt(st.nextToken());
+            int height = Integer.parseInt(st.nextToken());
+            arr[i][0] = weight;
+            arr[i][1] = height;
         }
-        for(int x = 0; x < n; x++){
+
+        for(int i = 0; i < n; i++){
             int count = 1;
-            for(int y = 0; y < n; y++){
-                if(x==y){
-                    continue;
-                }
-                if(arr[x][0]<arr[y][0] && arr[x][1] < arr[y][1]) {
+            for(int j = 0; j < n; j++){
+                if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]){
                     count++;
                 }
             }
-            System.out.print(count+" ");
+            System.out.print(count + " ");
         }
-    }
-}
+
+    }//main end
+}//class end

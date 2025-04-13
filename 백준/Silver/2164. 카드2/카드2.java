43a902e4ce9e5a78fcdaf6f1288ba41d262c42a2
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-04-13 16:54:43 +0900

    [Silver IV] Title: 카드2, Time: 124 ms, Memory: 23808 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,18 +1,26 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Queue<Integer> list = new LinkedList<>();
-
-        int n = sc.nextInt();
-        for(int i = 1; i <= n; i++){
-            list.add(i);
-        }
-        while(list.size() != 1){
-            list.poll();
-            list.offer(list.poll());
-        }
-        System.out.println(list.poll());
-    }
+	private static int n;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		Queue<Integer> q = new ArrayDeque<>();
+		//입력받은 수만큼 카드 채우기
+		for(int i = 1; i<= n; i++) {
+			q.offer(i);
+		}
+		//q의 사이즈가 1이 될때까지 첫장을 버리고 다음장을 맨뒤에 추가
+		while(q.size()>1) {
+			q.poll();
+			q.offer(q.poll());
+		}
+		
+		System.out.println(q.poll());
+	}
 }

47efb62c59147c234e29257471f3f0523fd4639e
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-04-16 16:43:05 +0900

    [Silver II] Title: 예산, Time: 104 ms, Memory: 13528 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,52 +1,44 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int n;
-    private static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-        int hight = 0;
-        int sum = 0;
+        int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++){
+
+        int[] arr = new int[n];
+        int maxValue = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            sum += arr[i];
-            if(hight < arr[i]){
-                hight = arr[i];
-            }
+            maxValue = Math.max(maxValue, arr[i]);
         }
 
         int m = Integer.parseInt(br.readLine());
-        
-        if(sum <= m){
-            System.out.println(hight);
-            return;
-        }
 
-        int min = 1;
-        while(min < hight - 1){
-            int mid = (min + hight)/2;
-            
-            if(cal(mid) > m){
-                hight = mid;
+        int left = 0;
+        int right = maxValue;
+        int answer = 0;
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            long total = 0;
+            for(int r : arr){
+                total += Math.min(r,mid);
+            }
+
+            if(total <= m){
+                answer = mid;
+                left = mid + 1;
             }else{
-                min = mid;
+                right = mid - 1;
             }
         }
-        System.out.println(min);
+
+        System.out.println(answer);
     }//main end
-    
-    private static int cal(int mid){
-        int total = 0;
-        for(int i = 0; i < n; i++){
-            total += Math.min(mid,arr[i]);
-        }
-        return total;
-    }
 }//class end

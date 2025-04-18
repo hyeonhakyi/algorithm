76f3277e518e6d932db5d671fc6329ac43ae6d32
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-04-18 18:38:47 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,18 +1,17 @@
-select he.EMP_NO as EMP_NO, he.EMP_NAME as EMP_NAME, 
-case
-    when avg(SCORE) >= 96 then 'S'
-    when avg(SCORE) >= 90 then 'A'
-    when avg(SCORE)>= 80 then 'B'
-else 'C'
-end as GRADE,
-case 
-    when avg(SCORE) >= 96 then (he.SAL * 0.2)
-    when avg(SCORE) >= 90 then (he.SAL * 0.15)
-    when avg(SCORE) >= 80 then (he.SAL * 0.1)
-else 0
-end as BONUS
-
+select he.EMP_NO, he.EMP_NAME,
+    case 
+        when avg(hg.SCORE) >= 96 then 'S'
+        when avg(hg.SCORE) >= 90 then 'A'
+        when avg(hg.SCORE) >= 80 then 'B'
+        else 'C'
+    end as GRADE,
+    case 
+        when avg(hg.SCORE) >= 96 then (he.SAL*0.2)
+        when avg(hg.SCORE) >= 90 then (he.SAL*0.15)
+        when avg(hg.SCORE) >= 80 then (he.SAL*0.10)
+        else 0
+    end as BONUS
 from HR_EMPLOYEES as he
 join HR_GRADE as hg on he.EMP_NO = hg.EMP_NO
 group by he.EMP_NO
-order by he.EMP_NO;
+order by he.EMP_NO

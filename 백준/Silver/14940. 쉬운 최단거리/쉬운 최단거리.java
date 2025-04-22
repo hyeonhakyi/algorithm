951b79c0199d1da8d7db13a11d20c2a404c2eaee
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-04-22 19:13:23 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 500 ms, Memory: 51352 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -6,78 +6,79 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static class Node{
+    static class Node{
         int x;
         int y;
         int count;
-        public Node(int x, int y,int count){
+        public Node(int x, int y, int count){
             this.x = x;
             this.y = y;
             this.count = count;
         }
     }
-    private static int[][] map;
-    private static int n,m,sx,sy;
-    private static int[] dx = {-1,1,0,0};
-    private static int[] dy = {0,0,-1,1};
-    private static boolean[][] visited;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static int n,m, sx,sy;
+    static int[][] answer;
+    static boolean[][] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
+        answer = new int[n][m];
         visited = new boolean[n][m];
 
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                answer[i][j] = Integer.parseInt(st.nextToken());
 
-                if(map[i][j] == 2){
+                if(answer[i][j] == 2){
                     sx = i;
                     sy = j;
-                    map[i][j] = 0;
+                    answer[i][j] = 0;
                 }
             }
         }
 
-        bfs();
+        cal();
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(!visited[i][j] && map[i][j] == 1){
-                    map[i][j] = -1;
+                if(!visited[i][j] && answer[i][j] == 1){
+                    answer[i][j] = -1;
                 }
-                System.out.print(map[i][j]+" ");
+                sb.append(answer[i][j]).append(" ");
             }
-            System.out.println();
+            sb.append("\n");
         }
+        System.out.println(sb);
     }//main end
-
-    private static void bfs(){
+    public static void cal(){
         Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(sx, sy,0));
+        q.add(new Node(sx, sy, 0));
         visited[sx][sy] = true;
 
         while(!q.isEmpty()){
-            Node now = q.poll();
+            Node cur = q.poll();
 
             for(int d = 0; d < 4; d++){
-                int nx = now.x + dx[d];
-                int ny = now.y + dy[d];
+                int nx = cur.x + dx[d];
+                int ny = cur.y + dy[d];
 
-                if(!check(nx,ny))continue;
-                if(visited[nx][ny])continue;
-                if(map[nx][ny] == 0)continue;
+                if(!check(nx,ny) || visited[nx][ny] || answer[nx][ny] == 0){
+                    continue;
+                }
 
-                q.offer(new Node(nx, ny, now.count+1));
+                q.add(new Node(nx, ny, cur.count+1));
                 visited[nx][ny] = true;
-                map[nx][ny] = now.count+1;
+                answer[nx][ny] = cur.count+1;
             }
         }
-    }//bfs end
+    }//cal end
 
-    private static boolean check(int x, int y){
+    public static boolean check(int x,int y){
         return x >= 0 && x < n && y >= 0 && y < m;
-    }//check end
+    }
 }//class end

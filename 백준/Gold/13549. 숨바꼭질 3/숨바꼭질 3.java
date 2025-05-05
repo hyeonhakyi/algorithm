185ea0c6d4dfecbfb4122d5c9ea13a50cddf3d3a
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-05-05 16:28:39 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 100 ms, Memory: 16676 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,61 +7,62 @@ import java.util.StringTokenizer;
 
 public class Main {
     static class Node{
-        int start;
-        int end;
-        public Node(int start,int end){
-            this.start = start;
-            this.end = end;
+        int x;
+        int count;
+        public Node(int x, int count){
+            this.x = x;
+            this.count = count;
         }
     }
-    static int n,k,ans;
-    static boolean[] visited = new boolean[100001];
-    static int time = Integer.MAX_VALUE;
+    static boolean[] check = new boolean[100001];
+    static int n,k,result;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
+        result = Integer.MAX_VALUE;
 
         if(n == k){
             System.out.println(0);
         }else{
-            bfs(n);
-            System.out.println(time);
+            bfs();
+            System.out.println(result);
         }
-    }
+    }//main end
 
-    static void bfs(int n){
-        Queue<Node> que = new LinkedList<>();
-        que.offer(new Node(n,0));
-        visited[n] = true;
+    private static void bfs(){
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(n, 0));
+        check[n] = true;
 
-        while(!que.isEmpty()){
-            Node now = que.poll();
+        while(!q.isEmpty()){
+            Node cur = q.poll();
+            int now = cur.x;
+            int nCount = cur.count;
 
-            if(now.start == k){
-                time = Math.min(time,now.end);
+            if(now == k){
+                result = Math.min(result, nCount);
             }
 
             int next;
-            next = now.start*2;
-
-            if(next < 100001 && !visited[next]){
-                visited[next] = true;
-                que.offer(new Node(next,now.end));
+            next = now * 2;
+            if(next < 100001 && !check[next]){
+                q.offer(new Node(next, nCount));
+                check[next] = true;
             }
 
-            next = now.start -1;
-            if(next >= 0 && !visited[next]){
-                visited[next] = true;
-                que.offer(new Node(next, now.end + 1 ));
+            next = now - 1;
+            if(next >= 0 && !check[next]){
+                q.offer(new Node(next, nCount + 1));
+                check[next] = true;
             }
 
-            next = now.start + 1;
-            if(next < 100001 && !visited[next]){
-                visited[next] = true;
-                que.offer(new Node(next, now.end + 1 ));
+            next = now + 1;
+            if(next < 100001 && !check[next]){
+                q.offer(new Node(next, nCount + 1));
+                check[next] = true;
             }
         }
-    }
-}
+    }//bfs end
+}//class end

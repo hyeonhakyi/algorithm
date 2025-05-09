be5a7e980d1e177c2f2d7736c23966f3e444267a
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-05-09 16:37:42 +0900

    [Gold V] Title: 탑, Time: 796 ms, Memory: 167172 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -4,44 +4,40 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-class Top{
-    int num;
-    int height;
-
-    Top(int num, int height){
-        this.height = height;
-        this.num = num;
-    }
-}
-
 public class Main {
+    static class Node{
+        int num;
+        int height;
+        public Node(int num, int height){
+            this.num = num;
+            this.height = height;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
-
-        Stack<Top> stack = new Stack<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        Stack<Node> stack = new Stack<Node>();
         StringBuilder sb = new StringBuilder();
-        st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= n; i++){
             int height = Integer.parseInt(st.nextToken());
-
+            
             if(stack.isEmpty()){
                 sb.append("0 ");
-                stack.push(new Top(i,height));
-            }else {
-                while (true) {
-                    if (stack.isEmpty()) {
+                stack.push(new Node(i, height));
+            }else{
+                while(true){
+                    if(stack.isEmpty()){
                         sb.append("0 ");
-                        stack.push(new Top(i, height));
+                        stack.push(new Node(i, height));
                         break;
                     }
 
-                    Top top = stack.peek();
-
-                    if(top.height > height){
-                        sb.append(top.num+" ");
-                        stack.push(new Top(i,height));
+                    Node node = stack.peek();
+                    if (node.height > height) {
+                        sb.append(node.num + " ");
+                        stack.push(new Node(i, height));
                         break;
                     }else{
                         stack.pop();
@@ -50,5 +46,5 @@ public class Main {
             }
         }
         System.out.println(sb.toString() + "\n");
-    }
-}
+    }//main end
+}//class end

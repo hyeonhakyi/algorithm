7772af090fc7a06ce030a72889ad8ec87b83d7ee
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-05-13 14:34:34 +0900

    [level 3] Title: 스타 수열, Time: 302.29 ms, Memory: 154 MB -BaekjoonHub

---
+++ 프로그래머스/3/70130. 스타 수열/스타 수열.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] a) {
+        int answer = -1;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < a.length; i++){
+            if(!map.containsKey(a[i])){
+                map.put(a[i], 1);
+            }else{
+                map.put(a[i],map.get(a[i]) + 1);
+            }
+        }
+        
+        for(int key : map.keySet()){
+            if(map.get(key) <= answer){
+                continue;
+            }
+            int count = 0;
+            
+            for(int i = 0; i < a.length - 1; i++){
+                if(a[i] != key && a[i + 1] != key){
+                    continue;
+                }
+                if(a[i] == a[i + 1]){
+                    continue;
+                }
+                count++;
+                i++;
+            }
+            answer = Math.max(answer,count);
+        }
+        return answer * 2;
+    }
+}

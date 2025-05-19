35a29797485aca6c1f9b8ff2daa9eea23c19dd17
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-05-19 16:47:32 +0900

    [Gold V] Title: 숫자고르기, Time: 64 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -2,24 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class Main {
-    private static int n;
-    private static int[] arr;
-    private static ArrayList<Integer> list = new ArrayList<>();
-    private static boolean[] visited;
+    static int[] arr;
+    static int n;
+    static boolean[] visited;
+    static List<Integer> list = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n+1];
-        visited = new boolean[n+1];
-
-        for(int i = 1; i <= n; i++){
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+        visited = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-
 
         for(int i = 1; i <= n; i++){
             visited[i] = true;
@@ -33,13 +31,15 @@ public class Main {
         }
     }//main end
 
-    private static void dfs(int start,int target){
+    private static void dfs(int start, int target) {
         if(!visited[arr[start]]){
             visited[arr[start]] = true;
             dfs(arr[start],target);
             visited[arr[start]] = false;
         }
-
-        if(arr[start] == target) list.add(target);
+            
+        if(arr[start] == target){
+            list.add(target);
+        }
     }//dfs end
 }//class end

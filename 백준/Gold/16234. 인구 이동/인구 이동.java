49773099eb51685dbea589e705c60de0aa9f701c
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-05-23 15:09:02 +0900

    [Gold IV] Title: 인구 이동, Time: 516 ms, Memory: 295328 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -7,21 +7,21 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static class Node{
+    static class Node{
         int x;
         int y;
-        public Node(int x,int y){
+        public Node(int x, int y){
             this.x = x;
             this.y = y;
         }
     }
-    private static boolean check;
-    private static int n,l,r,diff;
-    private static int[][] arr;
-    private static boolean[][] visited;
-    private static int[] dx = {-1,1,0,0};
-    private static int[] dy = {0,0,-1,1};
-    private static ArrayList<Node> list = new ArrayList<>();
+    static boolean check;
+    static int n,l,r;
+    static int[][] arr;
+    static boolean[][] visited;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static ArrayList<Node> list = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -30,66 +30,65 @@ public class Main {
         r = Integer.parseInt(st.nextToken());
         arr = new int[n][n];
         int day = 0;
-        for(int i = 0; i < n; i++){
+
+        for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < n; j++){
+            for(int j = 0; j < n; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        while (true){
+        while(true){
             check = false;
             visited = new boolean[n][n];
 
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
                     if(!visited[i][j]){
-                        bfs(i,j);
+                        bfs(i, j);
                     }
                 }
             }
-
             if(!check){
                 break;
-            }else {
+            }else{
                 day++;
             }
         }
         System.out.println(day);
     }//main end
 
-    private static void bfs(int x,int y){
-        Queue<Node> que = new LinkedList<>();
-        que.offer(new Node(x,y));
-        list.add(new Node(x,y));
+    public static void bfs(int x, int y){
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(x,y));
         visited[x][y] = true;
+        list.add(new Node(x,y));
+        int diff = 0;
 
-        while(!que.isEmpty()){
-            Node now = que.poll();
-            int nowX = now.x;
-            int nowY = now.y;
+        while(!q.isEmpty()){
+            Node now = q.poll();
 
-            for(int d = 0; d < 4; d++){
-                int nx = nowX + dx[d];
-                int ny = nowY + dy[d];
+            for(int d = 0; d < 4; d++) {
+                int nx = now.x + dx[d];
+                int ny = now.y + dy[d];
 
-                if(nx >= 0 && ny >= 0 && nx < n && ny < n){
-                    diff = Math.abs(arr[nowX][nowY] - arr[nx][ny]);
+                if(isCheck(nx,ny)){
+                    diff = Math.abs(arr[now.x][now.y] - arr[nx][ny]);
                     if(l <= diff && diff <= r){
                         if(!visited[nx][ny]){
                             check = true;
-                            que.offer(new Node(nx,ny));
-                            list.add(new Node(nx,ny));
+                            q.offer(new Node(nx,ny));
                             visited[nx][ny] = true;
+                            list.add(new Node(nx,ny));
                         }
                     }
                 }
             }
         }
-        priceChek();
+        isPrice();
     }//bfs end
 
-    private static void priceChek(){
+    private static void isPrice(){
         int sum = 0;
         for(int i = 0; i < list.size(); i++){
             Node node = list.get(i);
@@ -100,5 +99,9 @@ public class Main {
             arr[node.x][node.y] = sum/list.size();
         }
         list.clear();
-    }
+    }//isPrice end
+
+    private static boolean isCheck(int x, int y){
+        return x >= 0 && x < n && y >= 0 && y < n;
+    }//isCheck end
 }//class end

3a22a2a0009123b138ba25efa1fb4a4872763adb
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-05-30 15:12:37 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 156 ms, Memory: 17480 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -5,78 +5,78 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static class Node implements Comparable<Node> {
-		int x;
-		int y;
-		int value;
+    private static class Node implements Comparable<Node> {
+        int x;
+        int y;
+        int value;
+        public Node(int x, int y, int value){
+            this.x = x;
+            this.y = y;
+            this.value = value;
+        }
 
-		public Node(int x, int y, int value) {
-			this.x = x;
-			this.y = y;
-			this.value = value;
-		}
+        @Override
+        public int compareTo(Node o){
+            return Integer.compare(this.value, o.value);
+        }
+    }
+    static int n,result;
+    static int[][] arr;
+    static boolean[][] visited;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int idx = 1;
 
-		@Override
-		public int compareTo(Node o) {
-			return Integer.compare(this.value, o.value);
-		}
-	}
+        while(true) {
+            n = Integer.parseInt(br.readLine());
+            if(n == 0) break;
+            
+            arr = new int[n][n];
+            visited = new boolean[n][n];
+            result = 0;
+            
+            for(int i = 0; i < n; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for(int j = 0; j < n; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
 
-	private static int n, result;
-	private static int[][] arr;
-	private static boolean[][] visited;
-	private static int[] dx = { -1, 1, 0, 0 };
-	private static int[] dy = { 0, 0, -1, 1 };
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int idx = 1;
-		while (true) {
-			n = Integer.parseInt(br.readLine());
-			if(n == 0) {
-				break;
-			}
-			arr = new int[n][n];
-			visited = new boolean[n][n];
-			result = 0;
-			
-			for (int i = 0; i < n; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < n; j++) {
-					arr[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
+            bfs(0, 0);
+            System.out.println("Problem " + idx + ": " + result);
+            idx++;
+        }//while end
 
-			bfs(0, 0);
-			System.out.println("Problem "+ idx + ": " + result);
-			idx++;
-		}
-	}// main end
 
-	private static void bfs(int x, int y) {
-		PriorityQueue<Node> que = new PriorityQueue<>();
-		que.offer(new Node(x, y, arr[x][y]));
-		visited[x][y] = true;
-
-		while (!que.isEmpty()) {
-			Node now = que.poll();
-			int nowX = now.x;
-			int nowY = now.y;
-			int value = now.value;
-
-			if (nowX == n - 1 && nowY == n - 1) {
-				result = value;
-				return;
-			}
-
-			for (int d = 0; d < 4; d++) {
-				int nx = nowX + dx[d];
-				int ny = nowY + dy[d];
-
-				if (nx >= 0 && ny >= 0 && nx < n && ny < n && !visited[nx][ny]) {
-					visited[nx][ny] = true;
-					que.offer(new Node(nx, ny, value + arr[nx][ny]));
-				}
-			}
-		}
-	}
-}// call end
+    }//main end
+    
+    private static void bfs(int x, int y) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(x,y,arr[x][y]));
+        visited[x][y] = true;
+        
+        while(!pq.isEmpty()) {
+            Node node = pq.poll();
+            int nowX = node.x;
+            int nowY = node.y;
+            int value = node.value;
+            
+            if(nowX == n -1 && nowY == n - 1) {
+                result = value;
+                return;
+            }
+            
+            for(int d = 0; d < 4; d++) {
+                int newX = nowX + dx[d];
+                int newY = nowY + dy[d];
+                
+                if(newX >= 0 && newY >= 0 && newX < n && newY < n && !visited[newX][newY]) {
+                    visited[newX][newY] = true;
+                    pq.add(new Node(newX,newY,value + arr[newX][newY]));
+                }
+            }
+        }
+    }
+}//class end

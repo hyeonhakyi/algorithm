3736b9f5aad222eefb51eb696aa54f513bea6f0b
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-06-09 13:57:05 +0900

    [level 2] Title: 할인 행사, Time: 106.65 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,10 +3,11 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        Map<String, Integer> wantMap = new HashMap<String, Integer>();
+        
+        HashMap<String,Integer> wantMap = new HashMap<>();
         
         for(int i = 0; i < want.length; i++){
-            wantMap.put(want[i], number[i]);
+            wantMap.put(want[i],number[i]);
         }
         
         for(int j = 0; j < discount.length - 9; j++){
@@ -26,6 +27,8 @@ class Solution {
                 answer++;
             }
         }
+        
+        
         return answer;
     }
 }

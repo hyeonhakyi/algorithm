e3eaf3c4bf4e4b68983fda5aa5d46234a47277fe
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-06-11 14:36:20 +0900

    [level 2] Title: 피로도, Time: 3.09 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,25 +1,29 @@
 import java.util.*;
 
 class Solution {
-    public int answer;
-    public boolean[] visited;
+    static boolean[] visited;
+    static int answer;
     public int solution(int k, int[][] dungeons) {
+        answer = 0;
+        
         visited = new boolean[dungeons.length];
         
-        dfs(0,k,dungeons);
+        dfs(dungeons,k,0);
         
         return answer;
     }
     
-    
-    public void dfs(int depth, int k, int[][] dungeons){
+    static void dfs(int[][] dungeons,int k,int count){
+        answer = Math.max(answer,count);
+        
         for(int i = 0; i < dungeons.length; i++){
-            if(!visited[i] && dungeons[i][0] <= k){
-                visited[i] = true;
-                dfs(depth + 1, k - dungeons[i][1], dungeons);
-                visited[i] = false;
+            if(!visited[i]){
+                if(dungeons[i][0] <= k){
+                    visited[i] = true;
+                    dfs(dungeons,k - dungeons[i][1],count+1);
+                    visited[i] = false;
+                }
             }
         }
-        answer = Math.max(answer,depth);
-    }
+    }//dfs end
 }

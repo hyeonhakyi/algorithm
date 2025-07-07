1c691e1ce4978b340f1633608f307b84a687731b
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-07-07 16:23:45 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,6 +1,10 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME
-from ANIMAL_OUTS
-where ANIMAL_ID not in (select ANIMAL_ID
-                       from ANIMAL_INS )
-order by ANIMAL_ID, NAME
+SELECT
+    ANIMAL_ID,
+    NAME
+from
+    ANIMAL_OUTS
+where
+    ANIMAL_ID not in (select ANIMAL_ID
+                     from ANIMAL_INS)
+order by 
+    ANIMAL_ID

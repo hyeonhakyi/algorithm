428038281d95ce33fd6e4b18181e62660b56a1ad
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-07-14 16:15:04 +0900

    [level 2] Title: 시소 짝꿍, Time: 171.50 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -6,12 +6,12 @@ class Solution {
         
         Arrays.sort(weights);
         
-        Map<Double, Integer> map = new HashMap<>();
+        Map<Double,Integer> map = new HashMap<>();
         
         for(int w : weights){
             double a = w * 1.0;
             double b = (w * 2.0) / 3.0;
-            double c = (w * 1.0) / 2.0;
+            double c = (w * 2.0) / 4.0;
             double d = (w * 3.0) / 4.0;
             
             if(map.containsKey(a)) answer += map.get(a);
@@ -19,7 +19,7 @@ class Solution {
             if(map.containsKey(c)) answer += map.get(c);
             if(map.containsKey(d)) answer += map.get(d);
             
-            map.put((w * 1.0), map.getOrDefault((w * 1.0), 0) + 1);
+            map.put((w * 1.0), map.getOrDefault((w * 1.0),0) + 1);
         }
         
         return answer;

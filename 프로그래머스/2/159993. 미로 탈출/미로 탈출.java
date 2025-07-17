a6cac4e9e7e2ed6e347967133c43c57d1a00837d
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-07-17 15:50:33 +0900

    [level 2] Title: 미로 탈출, Time: 12.91 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
 class Node{
-        int x;
-        int y;
-        int cnt;
-        boolean check;
-        public Node(int x,int y,int cnt,boolean check){
-            this.x = x;
-            this.y = y;
-            this.cnt = cnt;
-            this.check = check;
-        }
+    int x;
+    int y;
+    int cnt;
+    boolean check;
+    public Node(int x,int y,int cnt,boolean check){
+        this.x = x;
+        this.y = y;
+        this.cnt = cnt;
+        this.check = check;
+    }
 }
 
 class Solution {
-    static char[][] arr;
-    static int n,m,sx,sy,lx,ly,ex,ey;
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
+    static char[][] arr;
+    static int n,m,sx,sy,lx,ly,ex,ey;
     public int solution(String[] maps) {
         int answer = 0;
         n = maps.length;
@@ -27,61 +27,65 @@ class Solution {
         for(int i = 0; i < n; i++){
             arr[i] = maps[i].toCharArray();
             for(int j = 0; j < m; j++){
-                if(arr[i][j] == 'S') {
+                if(arr[i][j] == 'S'){
                     sx = i;
                     sy = j;
-                }
-                if(arr[i][j] == 'L'){
+                }else if(arr[i][j] == 'L'){
                     lx = i;
                     ly = j;
-                }
-                if(arr[i][j] == 'E'){
+                }else if(arr[i][j] == 'E'){
                     ex = i;
                     ey = j;
                 }
             }
         }
-        answer = calculation();
+        answer = cal();
         
         return answer;
-    }//solution end
+    }
     
-    private static int calculation(){
+    public int cal(){
         Queue<Node> q = new LinkedList<>();
-        boolean[][][] visited = new boolean[n][m][2];
         q.offer(new Node(sx,sy,0,false));
+        boolean[][][] visited = new boolean[n][m][2];
         visited[sx][sy][0] = true;
         
         while(!q.isEmpty()){
             Node now = q.poll();
+            int nowX = now.x;
+            int nowY = now.y;
+        
+            
             
             for(int d = 0; d < 4; d++){
-                int nx = now.x + dx[d];
-                int ny = now.y + dy[d];
+                int nx = nowX + dx[d];
+                int ny = nowY + dy[d];
                 int cnt = now.cnt + 1;
-                boolean che = now.check;
+                boolean nowCheck = now.check;
                 int checkL = 0;
                 
-                if(nx == ex && ny == ey && che == true){
-                    return now.cnt + 1;
+                if(nowCheck == true && nx == ex && ny == ey){
+                    return cnt;
                 }
                 
                 if(!check(nx,ny)) continue;
-                if(arr[nx][ny] == 'L' || che){
-                    che = true;
+                
+                if(arr[nx][ny] == 'L' || nowCheck){
+                    nowCheck = true;
                     checkL = 1;
                 }
+                
                 if(visited[nx][ny][checkL]) continue;
                 if(arr[nx][ny] == 'X') continue;
                 
                 visited[nx][ny][checkL] = true;
-                q.offer(new Node(nx,ny,cnt,che));
+                q.offer(new Node(nx,ny,cnt,nowCheck));
             }
         }
         return -1;
-    }//calculation end
+    }//cal end
     
-    private static boolean check(int x,int y){
+    public boolean check(int x, int y){
         return x >= 0 && x < n && y >= 0 && y < m;
     }
 }

992885551638325eaf3da00973297d63b93181f0
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-07-19 17:26:33 +0900

    [level 2] Title: 배달, Time: 3.42 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,69 +1,71 @@
 import java.util.*;
 
 class Node implements Comparable<Node>{
-        int idx;
-        int weight;
-        public Node(int idx,int weight){
-            this.idx = idx;
-            this.weight = weight;
-        }
+    int idx;
+    int weight;
     
-        @Override
-        public int compareTo(Node o){
-            return Integer.compare(this.weight,o.weight);
-        }
+    public Node(int idx,int weight){
+        this.idx = idx;
+        this.weight = weight;
     }
+    
+    @Override
+    public int compareTo(Node o){
+        return Integer.compare(this.weight,o.weight);
+    }
+}
 
 class Solution {
     public int solution(int N, int[][] road, int K) {
         int answer = 1;
         
-        ArrayList<Node>[] list = new ArrayList[N+1];;
+        ArrayList<Node>[] list = new ArrayList[N+1];
+        
         for(int i = 1; i <= N; i++){
             list[i] = new ArrayList();
         }
         
-        for(int i = 0; i < road.length; i++){
-            int from = road[i][0];
-            int to = road[i][1];
-            int value = road[i][2];
+        for(int[] r : road){
+            int s = r[0];
+            int e = r[1];
+            int w = r[2];
             
-            list[from].add(new Node(to,value));
-            list[to].add(new Node(from,value));
+            list[s].add(new Node(e,w));
+            list[e].add(new Node(s,w));
         }
         
-        int[] dist = dijkstra(1,N,K,list);
+        int[] dist = cal(1,N,list,K);
         
         for(int i = 2; i <= N; i++){
             if(dist[i] <= K){
                 answer++;
             }
         }
-        
+
         return answer;
     }
     
-    static int[] dijkstra(int start,int n,int k,ArrayList<Node>[] list){
-        PriorityQueue<Node> que = new PriorityQueue<>();
-        que.offer(new Node(start,0));
+    public int[] cal(int start,int n,ArrayList<Node>[] list,int k){
+        PriorityQueue<Node> q = new PriorityQueue<>();
+        q.offer(new Node(start,0));
         boolean[] visited = new boolean[n+1];
         int[] dist = new int[n+1];
         Arrays.fill(dist,Integer.MAX_VALUE);
         dist[start] = 0;
-    
-        while(!que.isEmpty()){
-            Node now = que.poll();  
+        
+        while(!q.isEmpty()){
+            Node now = q.poll();
             
-            if(visited[now.idx])continue;
+            if(visited[now.idx]) continue;
             visited[now.idx] = true;
             
             for(Node next : list[now.idx]){
                 if(dist[next.idx] > dist[now.idx] + next.weight){
                     dist[next.idx] = dist[now.idx] + next.weight;
-                    que.offer(new Node(next.idx,dist[next.idx]));
-                } 
-            }  
+                    q.offer(new Node(next.idx,dist[next.idx]));
+                }
+            }
         }
         return dist;
-    }
+    }//cal end
 }

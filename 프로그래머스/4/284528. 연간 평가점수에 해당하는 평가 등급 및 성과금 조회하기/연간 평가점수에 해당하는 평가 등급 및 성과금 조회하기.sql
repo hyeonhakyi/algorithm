404e3ba0ec51514b2f53620a85e803fff869e475
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-07-21 15:06:14 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,18 +1,26 @@
 -- 코드를 작성해주세요
-select e.EMP_NO, e.EMP_NAME,
+select
+    e.EMP_NO,
+    e.EMP_NAME,
     case 
         when avg(g.SCORE) >= 96 then 'S'
         when avg(g.SCORE) >= 90 then 'A'
         when avg(g.SCORE) >= 80 then 'B'
         else 'C'
     end as GRADE,
-    case 
-        when avg(g.SCORE) >= 96 then (e.SAL * 0.2)
-        when avg(g.SCORE) >= 90 then (e.SAL * 0.15)
-        when avg(g.SCORE) >= 80 then (e.SAL * 0.1)
-        else (e.SAL * 0)
+    case
+        when avg(g.SCORE)  >= 96 then e.SAL * 0.2
+        when avg(g.SCORE)  >= 90 then e.SAL * 0.15
+        when avg(g.SCORE)  >= 80 then e.SAL * 0.1
+        else 0
     end as BONUS
-from HR_EMPLOYEES as e
-join HR_GRADE as g on e.EMP_NO = g.EMP_NO
-group by e.EMP_NO
-order by e.EMP_NO
+from
+    HR_EMPLOYEES as e
+join
+    HR_DEPARTMENT as d on e.DEPT_ID = d.DEPT_ID
+join
+    HR_GRADE as g on e.EMP_NO = g.EMP_NO
+group by
+    e.EMP_NO
+order by    
+    e.EMP_NO

490b8fdbb4352f85cae5f08e433274d3a3abdd79
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-07-23 17:19:33 +0900

    [level 2] Title: 디펜스 게임, Time: 102.90 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int k, int[] enemy) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b) -> b - a); 
-        int round = 0;
+        int answer = 0;
         
-        for(int e : enemy){
-           maxHeap.add(e); // 현재 라운드의 적의 수를 저장
-           n -= e; // 병사 이용
+        PriorityQueue<Integer> q = new PriorityQueue<Integer>((a,b) -> b - a);
+        
+        for(int i : enemy){
+            q.offer(i);
+            n -= i;
             
-           // 병사가 부족하면 무적권 사용
-           if(n < 0){
-               if(k > 0){
-                   n += maxHeap.poll(); // 가장 큰 무리 찬스권 사용
-                   k--; // 찬스권 제거
-               }else{
-                   break;
-               }
-           }            
-           round++; // 라운드 증가
+            if(n < 0){
+                if(k > 0){
+                    n += q.poll();
+                    k--;
+                }else{
+                    break;
+                }
+            }
+            answer++;
         }
         
         
-        return round;
+        return answer;
     }
 }

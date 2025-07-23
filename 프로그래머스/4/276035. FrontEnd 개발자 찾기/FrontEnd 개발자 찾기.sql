7e1ecf7cf6a39fb8a6e79c9eb74791241ee78ee2
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-07-23 14:06:11 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,6 +1,13 @@
--- 코드를 작성해주세요
-select distinct d.ID, d.EMAIL,d.FIRST_NAME, d.LAST_NAME
-from SKILLCODES as s
-join DEVELOPERS as d ON s.CODE = d.SKILL_CODE&s.CODE
-where s.CATEGORY like 'Front End'
-order by d.ID
+select
+    distinct d.ID,
+    d.EMAIL,
+    d.FIRST_NAME,
+    d.LAST_NAME
+from
+    SKILLCODES as s
+join
+    DEVELOPERS as d on (s.CODE & d.SKILL_CODE) = s.CODE
+where
+    s.CATEGORY like 'Front End'
+order by
+    ID

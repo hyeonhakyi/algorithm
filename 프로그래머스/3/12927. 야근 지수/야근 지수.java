3829c7324d00a00faf442921ee68a0b712e8b895
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-07-26 14:45:16 +0900

    [level 3] Title: 야근 지수, Time: 130.88 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -3,27 +3,30 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] works) {
         long answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Integer> q = new PriorityQueue<>(Comparator.reverseOrder());
         
-        for(int i = 0; i < works.length; i++){
-            pq.offer(works[i]);
+        for(int i : works){
+            q.offer(i);
         }
         
         while(n > 0){
-            int now = pq.poll();
+            int now = q.poll();
+            
             if(now == 0){
                 break;
             }
             n--;
-            pq.offer(now - 1);
+            q.offer(now - 1);
         }
         
-        while(!pq.isEmpty()){
-            int now = pq.poll();
-            
-            answer += now * now;
+        if(q.isEmpty()){
+            return 0;
+        }else{
+            while(!q.isEmpty()){
+                int now = q.poll();
+                answer += now * now;
+            }
         }
-        
         
         return answer;
     }

319dc30b1a69613749d0a6ad5e75741fc0863669
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-07-29 16:47:41 +0900

    [level 3] Title: 최고의 집합, Time: 0.29 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,19 +1,18 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int n, int s) {
-        if(s < n){
-            return new int[]{-1};
-        }
-        
+        if(s < n) return new int[] {-1};
         int[] answer = new int[n];
         
-        int base = s/n;
-        int remainder = s%n;
+        int first = s / n;
+        int second = s % n;
         
         for(int i = 0; i < n; i++){
-            answer[i] = base;
+            answer[i] = first;
         }
         
-        for(int i = n - 1; i >= n - remainder; i--){
+        for(int i = n - 1; i >= n - second; i--){
             answer[i]++;
         }
         

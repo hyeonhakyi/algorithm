18c61ebbc138b9ffb503211a4ea0cf183ef3b7ca
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-08-07 15:40:01 +0900

    [level 3] Title: 여행경로, Time: 82.80 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -4,7 +4,7 @@ class Solution {
     static List<String> answer;
     static boolean[] visited;
     public String[] solution(String[][] tickets) {
-        answer = new LinkedList<>();
+        answer = new ArrayList<>();
         visited = new boolean[tickets.length];
         
         Arrays.sort(tickets, (a,b) -> a[1].compareTo(b[1]));
@@ -12,29 +12,32 @@ class Solution {
         List<String> path = new LinkedList<>();
         path.add("ICN");
         
-        dfs("ICN",tickets,path,0);
+        bfs("ICN",path,tickets,0);
+        
         
         return answer.toArray(new String[0]);
     }
     
-    private static void dfs(String currrnt,String[][] tickets,List<String> path,int ueseTickets){
-        if(ueseTickets == tickets.length){
+    static void bfs(String start,List<String> path,String[][] tickets,int use){
+        if(use == tickets.length){
             if(answer.isEmpty()){
                 answer = new LinkedList<>(path);
-                return;
             }
+            return;
         }
         
         for(int i = 0; i < tickets.length; i++){
-            if(!visited[i] && tickets[i][0].equals(currrnt)){
+            if(!visited[i] && start.equals(tickets[i][0])){
                 visited[i] = true;
                 path.add(tickets[i][1]);
                 
-                dfs(tickets[i][1], tickets,path,ueseTickets + 1);
+                bfs(tickets[i][1],path,tickets,use+1);
                 
                 visited[i] = false;
                 path.remove(path.size() - 1);
             }
         }
-    }//dfs end
+        
+        
+    }//bfs end
 }

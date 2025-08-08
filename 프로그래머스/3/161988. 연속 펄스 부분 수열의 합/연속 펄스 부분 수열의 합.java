f94efd82315be91db5640dc3954e8add495fb33d
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-08-08 16:03:07 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 18.69 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -10,14 +10,13 @@ class Solution {
         long p2 = 0;
         
         for(int s : sequence){
-            p1 += check ? s : -s;
-            p2 += check ? -s : s;
+            p1 += check ? -s : s;
+            p2 += check ? s : -s;
             
-            p1 = Math.max(0,p1);
-            p2 = Math.max(0,p2);
+            p1 = Math.max(0, p1);
+            p2 = Math.max(0, p2);
             
             answer = Math.max(answer,Math.max(p1,p2));
-            
             check = !check;
         }
         

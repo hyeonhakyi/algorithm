ca176b1985d803f0ef860a7d4a558085c4b79321
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-08-11 17:06:51 +0900

    [level 3] Title: 부대복귀, Time: 253.86 ms, Memory: 177 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -2,38 +2,41 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-        List<List<Integer>> gr = new ArrayList<>();
+        int[] answer = new int[sources.length];
+        
+        ArrayList<Integer>[] list = new ArrayList[n+1];
         
         for(int i = 0; i <= n; i++){
-            gr.add(new ArrayList<>());
+            list[i] = new ArrayList<>();
         }
         
-        for(int[] road : roads){
-            int n1 = road[0];
-            int n2 = road[1];
-            gr.get(n1).add(n2);
-            gr.get(n2).add(n1);
+        for(int[] i : roads){
+            int start = i[0];
+            int end = i[1];
+            
+            list[start].add(end);
+            list[end].add(start);
         }
         
-        int[] dist = new int[n+1];
+        int[] dist = new int[n + 1];
+        
         Arrays.fill(dist,-1);
+        dist[destination] = 0;
         
         Queue<Integer> q = new LinkedList<>();
         q.offer(destination);
-        dist[destination] = 0;
         
         while(!q.isEmpty()){
             int now = q.poll();
             
-            for(int next : gr.get(now)){
+            for(int next : list[now]){
                 if(dist[next] == -1){
-                    dist[next] = dist[now] + 1;
                     q.offer(next);
+                    dist[next] = dist[now] + 1;
                 }
             }
         }
         
-        int[] answer = new int[sources.length];
         for(int i = 0; i < sources.length; i++){
             answer[i] = dist[sources[i]];
         }

4cfdd63eb52de476ea482dd71408e7e50e61add9
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-08-12 14:10:02 +0900

    [level 3] Title: 입국심사, Time: 54.02 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,29 +1,30 @@
+import java.util.*;
+
 class Solution {
-    private long answer;
-    
+    static long answer;
     public long solution(int n, int[] times) {
         long maxTime = (long)1000000000 * (long)1000000000;
         long minTime = 1;
         answer = maxTime;
-        search(times,n,minTime,maxTime);
+        search(n,times,minTime,maxTime);
         return answer;
     }
     
-    private void search(int[] times, int goal, long start, long end){
+    static void search(int n,int[] times,long start,long end){
         while(start <= end){
             long mid = (start + end) / 2;
-            
             long timeCnt = 0;
-            for(int time : times){
-                timeCnt += (mid / time);
+            
+            for(int t : times){
+                timeCnt += (mid / t);
             }
             
-            if(goal <= timeCnt){
+            if(n <= timeCnt){
                 answer = Math.min(answer,mid);
                 end = mid - 1;
             }else{
                 start = mid + 1;
             }
         }
-    }
+    }//search end
 }

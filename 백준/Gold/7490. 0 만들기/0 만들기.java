3082895d9a4437ea291006530cf3d2729db45437
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-08-16 16:37:57 +0900

    [Gold V] Title: 0 만들기, Time: 76 ms, Memory: 15184 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.java
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -1,32 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main {
-    static int n;
+    static int target;
     static StringBuilder sb;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+        int n  = Integer.parseInt(br.readLine());
 
-        for(int i = 1; i <= t; i++) {
-            n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            target = Integer.parseInt(br.readLine());
             sb = new StringBuilder();
+
             dfs(1,1,0,1,"1");
             System.out.println(sb);
         }
+
+
     }//main end
 
-    private static void dfs(int idx, int num,int sum, int op, String ex) {
-        if(idx == n) {
+    static void dfs(int idx,int num,int sum,int op,String ex){
+        if(idx == target){
             sum += (num*op);
             if(sum == 0){
-                sb.append(ex + "\n");
+                sb.append(ex+"\n");
             }
             return;
         }
-
         dfs(idx + 1, num * 10 + (idx + 1), sum, op, ex + " " + Integer.toString(idx + 1));
         dfs(idx +1, idx +1, sum + (num * op), 1, ex + "+"+ Integer.toString(idx + 1));
         dfs(idx + 1, idx + 1, sum + (num * op), -1, ex + "-" + Integer.toString(idx + 1));

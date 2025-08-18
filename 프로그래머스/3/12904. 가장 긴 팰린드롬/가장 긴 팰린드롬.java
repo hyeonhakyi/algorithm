fdb737b0a84305cf0675d2a144de0f061d98612e
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-08-18 15:23:20 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 18.07 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -1,22 +1,25 @@
 import java.util.*;
 
+
 class Solution{
     public int solution(String s){
-        int answer = 1;
+        int answer = 0;
         
         for(int i = 0; i < s.length(); i++){
-            answer = Math.max(answer,check(s,i,i));
+            answer = Math.max(answer,cal(s,i,i));
             
-            answer = Math.max(answer,check(s,i,i+1));
+            answer = Math.max(answer,cal(s,i,i+1));
         }
+
         return answer;
-    }
+    }//main end
     
-    private static int check(String s,int left,int right){
+    public int cal(String s,int left,int right){
         while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
             left--;
             right++;
         }
+        
         return right - left - 1;
-    }
-}
+    }//cal end
+}//class end

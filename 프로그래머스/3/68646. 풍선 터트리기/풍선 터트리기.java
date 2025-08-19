bd756bb1b6d863faf11b9df9882549a5677d7a60
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-08-19 16:32:48 +0900

    [level 3] Title: 풍선 터트리기, Time: 47.19 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -5,31 +5,29 @@ class Solution {
         int n = a.length;
         int[] leftMin = new int[n];
         int[] rightMin = new int[n];
-        
+
+        // 왼쪽 최소값
         int min = Integer.MAX_VALUE;
-        for(int i = 0; i < n; i++){
-            min = Math.min(min,a[i]);
+        for (int i = 0; i < n; i++) {
+            min = Math.min(min, a[i]);
             leftMin[i] = min;
         }
-        
+
+        // 오른쪽 최소값
         min = Integer.MAX_VALUE;
-        for(int i = n - 1; i >= 0; i--){
-            min = Math.min(min,a[i]);
+        for (int i = n - 1; i >= 0; i--) {
+            min = Math.min(min, a[i]);
             rightMin[i] = min;
         }
-        
+
         int answer = 0;
-        for(int i = 0; i < n; i++){
-            if(i == 0 || i == n-1){
-                answer++;
-                continue;
-            }
-            
-            if(a[i] < leftMin[i - 1] || a[i] < rightMin[i + 1]){
+        for (int i = 0; i < n; i++) {
+            // 자신이 왼쪽 최소 or 오른쪽 최소면 생존 가능
+            if (a[i] <= leftMin[i] || a[i] <= rightMin[i]) {
                 answer++;
             }
         }
-        
+
         return answer;
     }
 }

4a6f75822be4f8a8862ac8d77c160a41ee1a3508
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-08-21 15:47:03 +0900

    [level 3] Title: 순위, Time: 23.63 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -3,36 +3,38 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] results) {
         int answer = 0;
-        boolean[][] check = new boolean[n+1][n+1];
+        int[][] check = new int[n+1][n+1];
         
-        for(int[] result : results){
-            check[result[0]][result[1]] = true;
+        for(int[] r : results){
+            int win = r[0];
+            int lose = r[1];
+            check[win][lose] = 1;
+            check[lose][win] = -1;
         }
         
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                for(int k = 1; k <= n; k++){
-                    if(check[j][i] && check[i][k]){
-                        check[j][k] = true;
+        
+        for(int b = 1; b <= n; b++){
+            for(int a = 1; a <= n; a++){
+                for(int c = 1; c <= n; c++){
+                    if(check[a][b] == 1 && check[b][c] == 1){
+                        check[a][c] = 1;
+                        check[c][a] = -1;
+                    }else if(check[a][b] == -1 && check[b][c] == -1){
+                        check[a][c] = -1;
+                        check[c][a] = 1;
                     }
                 }
             }
         }
         
-        int count = 0;
         for(int i = 1; i <= n; i++){
-            count = 0;
+            int cnt = 0;
             for(int j = 1; j <= n; j++){
                 if(i == j) continue;
-                if(check[i][j] || check[j][i]){
-                    count++;
-                }
+                if(check[i][j] != 0) cnt++;
             }
-            if(count == n-1){
-                answer++;
-            }
+            if(cnt == n - 1) answer++;
         }
-        
         
         return answer;
     }

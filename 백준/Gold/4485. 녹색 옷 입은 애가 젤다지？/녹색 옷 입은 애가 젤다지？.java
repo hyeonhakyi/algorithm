723b5cf0cdae4042c1060890d1f5399279e83d3c
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-08-22 16:09:21 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 140 ms, Memory: 16848 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,82 +1,87 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static class Node implements Comparable<Node> {
+    static class Node implements Comparable<Node> {
         int x;
         int y;
         int value;
-        public Node(int x, int y, int value){
+        public Node(int x, int y, int value) {
             this.x = x;
             this.y = y;
             this.value = value;
         }
 
         @Override
-        public int compareTo(Node o){
+        public int compareTo(Node o) {
             return Integer.compare(this.value, o.value);
         }
     }
-    static int n,result;
     static int[][] arr;
-    static boolean[][] visited;
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
+    static boolean[][] visited;
+    static int n;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int idx = 1;
 
-        while(true) {
+        int idx = 0;
+        while(true){
+            idx++;
             n = Integer.parseInt(br.readLine());
             if(n == 0) break;
-            
+
             arr = new int[n][n];
             visited = new boolean[n][n];
-            result = 0;
-            
-            for(int i = 0; i < n; i++) {
+
+            for (int i = 0; i < n; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                for(int j = 0; j < n; j++) {
+                for (int j = 0; j < n; j++) {
                     arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            bfs(0, 0);
-            System.out.println("Problem " + idx + ": " + result);
-            idx++;
-        }//while end
+            int answer = bfs();
 
-
+            System.out.println("Problem" + " " + idx + ": " + answer);
+        }
     }//main end
-    
-    private static void bfs(int x, int y) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(x,y,arr[x][y]));
-        visited[x][y] = true;
-        
-        while(!pq.isEmpty()) {
-            Node node = pq.poll();
-            int nowX = node.x;
-            int nowY = node.y;
-            int value = node.value;
-            
-            if(nowX == n -1 && nowY == n - 1) {
-                result = value;
-                return;
+
+    static int bfs() {
+        PriorityQueue<Node> q = new PriorityQueue<>();
+        q.offer(new Node(0, 0, arr[0][0]));
+        visited[0][0] = true;
+
+        while(!q.isEmpty()) {
+            Node now = q.poll();
+            int nowX = now.x;
+            int nowY = now.y;
+            int nowValue = now.value;
+
+            if(nowX == n-1 && nowY == n-1){
+                return nowValue;
             }
-            
-            for(int d = 0; d < 4; d++) {
-                int newX = nowX + dx[d];
-                int newY = nowY + dy[d];
-                
-                if(newX >= 0 && newY >= 0 && newX < n && newY < n && !visited[newX][newY]) {
-                    visited[newX][newY] = true;
-                    pq.add(new Node(newX,newY,value + arr[newX][newY]));
-                }
+
+            for(int d = 0; d < 4; d++){
+                int nx = now.x + dx[d];
+                int ny = now.y + dy[d];
+
+                if(!check(nx,ny)) continue;
+                if(visited[nx][ny]) continue;
+
+                visited[nx][ny] = true;
+                q.offer(new Node(nx,ny,nowValue + arr[nx][ny]));
             }
         }
-    }
+        return -1;
+    }//bfs end
+
+    static boolean check(int x,int y){
+        return x >= 0 && x < n && y >= 0 && y < n;
+    }//check end
 }//class end

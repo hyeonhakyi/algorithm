ccab87aee50c724936bc6d7243e9431b343eeeea
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-09-04 16:17:15 +0900

    [Gold IV] Title: 문자열 폭발, Time: 352 ms, Memory: 37768 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -6,38 +6,40 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        String bom = br.readLine();
-        int bomSize = bom.length();
+        String s = br.readLine();
+        String target = br.readLine();
+        int bomSize = target.length();
 
-        Stack<Character> st = new Stack<>();
+        Stack<Character> stack = new Stack<>();
 
-        for(int i = 0; i < str.length(); i++) {
-            st.push(str.charAt(i));
-            if(st.size() >= bomSize){
+        for(int i = 0; i < s.length(); i++){
+            stack.push(s.charAt(i));
+
+            if(stack.size() >= bomSize){
                 boolean check = true;
-
-                for(int j = 0; j < bomSize; j++) {
-                    if(st.get(st.size() - bomSize + j) != bom.charAt(j)){
+                for(int j = 0; j < bomSize; j++){
+                    if(stack.get(stack.size() - bomSize + j) != target.charAt(j)){
                         check = false;
                         break;
                     }
                 }
+
                 if(check){
-                    for(int j = 0; j < bomSize; j++) {
-                        st.pop();
+                    for(int j = 0; j < bomSize; j++){
+                        stack.pop();
                     }
                 }
             }
         }
-        if(st.isEmpty()){
+
+        if(stack.isEmpty()){
             System.out.println("FRULA");
         }else{
             StringBuilder sb = new StringBuilder();
-            for(Character c : st){
-                sb.append(c);
+            for(char x : stack){
+                sb.append(x);
             }
-            System.out.println(sb);
+            System.out.println(sb.toString());
         }
     }//main end
 }//class end

6254560b781e6d78d689f329e24fe81b199d0788
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-09-29 16:20:07 +0900

    [level 2] Title: 할인 행사, Time: 99.70 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -4,31 +4,32 @@ class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         
-        HashMap<String,Integer> wantMap = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
         
         for(int i = 0; i < want.length; i++){
-            wantMap.put(want[i],number[i]);
+            map.put(want[i],number[i]);
         }
         
         for(int j = 0; j < discount.length - 9; j++){
             int idx = 0;
-            Map<String, Integer> map = new HashMap<String, Integer>();
+            Map<String,Integer> disMap = new HashMap<String,Integer>();
             
             for(int i = 0; i < 10; i++){
-                map.put(discount[i+j], map.getOrDefault(discount[i+j],0) + 1);
+                disMap.put(discount[i + j], disMap.getOrDefault(discount[i + j],0) + 1);
             }
             
-            for(Map.Entry<String, Integer> entry : wantMap.entrySet()){
-                if(map.containsKey(entry.getKey()) && entry.getValue() <= map.get(entry.getKey())){
+            for(Map.Entry<String,Integer> entry : map.entrySet()){
+                if(disMap.containsKey(entry.getKey()) && entry.getValue() <= disMap.get(entry.getKey())){
                     idx++;
                 }
             }
+            
             if(idx == want.length){
                 answer++;
             }
         }
         
-        
+      
         return answer;
     }
 }

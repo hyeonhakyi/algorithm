a0445fd642ee8c6f4dfbae580abade5dcb4cfc16
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-09-30 17:21:47 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -6,16 +6,12 @@ class Solution {
         
         HashMap<String,Integer> map = new HashMap<>();
         
-        for(String[] cloth : clothes){
-            if(!map.containsKey(cloth[1])){
-                map.put(cloth[1],1);
-            }else{
-                map.put(cloth[1],map.get(cloth[1]) + 1);
-            }
+        for(String[] clothe : clothes){
+            map.put(clothe[1],map.getOrDefault(clothe[1],0) + 1);
         }
         
-        for(int value : map.values()){
-            answer *= value + 1;
+        for(int i : map.values()){
+            answer *= i + 1;
         }
         
         return answer - 1;

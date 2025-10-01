b9a69611bf1e0fcd9ebed49c5ae07f68b7316394
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-10-01 17:46:05 +0900

    [level 2] Title: 기능개발, Time: 0.37 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 75.8 MB, 시간: 0.13 ms
+메모리: 69.8 MB, 시간: 0.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 16일 21:48:28
+2025년 10월 01일 17:46:03
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,33 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> que = new LinkedList<>();
-        List<Integer> answer = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        Stack<Integer> answer = new Stack<>();
         
         for(int i = 0; i < progresses.length; i++){
             if((100 - progresses[i]) % speeds[i] == 0){
-                que.offer((100-progresses[i]) / speeds[i]);
+                q.offer((100 - progresses[i]) / speeds[i]);
             }else{
-                que.offer(((100 - progresses[i]) / speeds[i] +1));
+                q.offer((100 - progresses[i]) / speeds[i] +1);
             }
         }
         
-        int now = que.poll();
+        int now = q.poll();
         int count = 1;
         
-        while(!que.isEmpty()){
-            if(now >= que.peek()){
+        while(!q.isEmpty()){
+            if(now >= q.peek()){
                 count++;
-                que.poll();
+                q.poll();
             }else{
                 answer.add(count);
+                now = q.poll();
                 count = 1;
-                now = que.poll();
             }
         }
         
         answer.add(count);
-        
         int[] in = new int[answer.size()];
         
         for(int i = 0; i < answer.size(); i++){

638e8a5e67a837229d5b200a1f0897b7fa9cfd9a
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-10-10 15:28:48 +0900

    [level 2] Title: 튜플, Time: 79.96 ms, Memory: 99.8 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/README.md
+++ 프로그래머스/2/64065. 튜플/README.md
@@ -1,10 +1,10 @@
 # [level 2] 튜플 - 64065 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065) 
 
 ### 성능 요약
 
-메모리: 98.8 MB, 시간: 66.39 ms
+메모리: 99.8 MB, 시간: 79.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 02일 16:51:35
+2025년 10월 10일 15:28:47
 
 ### 문제 설명
 
--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,15 +2,15 @@ import java.util.*;
 
 class Solution {
     public List<Integer> solution(String s) {
-        s = s.substring(2, s.length() - 2).replace("},{", "-");
-        String[] arr = s.split("-");
+        s = s.substring(2,s.length() - 2).replace("},{","-");
+        String[] str = s.split("-");
         
-        Arrays.sort(arr, Comparator.comparingInt(String::length));
+        Arrays.sort(str, Comparator.comparingInt(String::length));
         
         List<Integer> list = new ArrayList<>();
         
-        for (String st : arr){
-            String[] check = st.split(",");
+        for(String arr : str){
+            String[] check = arr.split(",");
             
             for(int i = 0; i < check.length; i++){
                 int num = Integer.parseInt(check[i]);

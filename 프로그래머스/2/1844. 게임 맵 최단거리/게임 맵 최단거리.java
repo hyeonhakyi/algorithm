fcb5fa13bbcba0dea9717a22fa7a21fcbe391f66
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-10-14 15:56:15 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.73 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -14,22 +14,18 @@ class Node{
 class Solution {
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
-    static int answer, n, m;
+    static int n,m;
     public int solution(int[][] maps) {
-        answer = Integer.MAX_VALUE;
+        int answer = 0;
         n = maps.length;
         m = maps[0].length;
         
-        bfs(maps);
+        answer = bfs(maps);
         
-        if(answer == Integer.MAX_VALUE){
-            return -1;
-        }else{
-            return answer;    
-        }
+        return answer;
     }
     
-    static void bfs(int[][] maps){
+    public static int bfs(int[][] maps){
         Queue<Node> q = new LinkedList<>();
         q.offer(new Node(0,0,1));
         boolean[][] visited = new boolean[n][m];
@@ -38,30 +34,27 @@ class Solution {
         while(!q.isEmpty()){
             Node now = q.poll();
             
+            if((now.x == n - 1) && (now.y == m - 1)){
+                return now.cnt;
+            }
+            
             for(int d = 0; d < 4; d++){
                 int nx = now.x + dx[d];
                 int ny = now.y + dy[d];
                 
-                if((nx == n - 1) && (ny == m - 1)){
-                    answer = Math.min(answer,now.cnt + 1);
-                    continue;
-                }
+                if(!check(nx,ny)) continue;
+                if(maps[nx][ny] == 0) continue;
+                if(visited[nx][ny]) continue;
                 
-                if(!check(nx,ny) || visited[nx][ny]){
-                    continue;
-                }
-                
-                if(maps[nx][ny] == 0){
-                    continue;
-                }
-                
-                q.offer(new Node(nx,ny,now.cnt+1));
+                q.offer(new Node(nx,ny,now.cnt + 1));
                 visited[nx][ny] = true;
             }
         }
-    }//bfs end
+        
+        return -1;
+    }
     
-    static boolean check(int x, int y){
+    static boolean check(int x,int y){
         return x >= 0 && x < n && y >= 0 && y < m;
     }
 }

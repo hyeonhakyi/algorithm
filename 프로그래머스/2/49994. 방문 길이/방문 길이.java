4212b8c18e0c6b525148437e9fa1dfb0cd5cbc1c
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-10-15 16:26:51 +0900

    [level 2] Title: 방문 길이, Time: 17.14 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -2,41 +2,40 @@ import java.util.*;
 
 class Solution {
     public int solution(String dirs) {
-        int x = 0;
-        int y = 0;
+        int answer = 0;
         
         Set<String> set = new HashSet<>();
         
-        int answer = 0;
+        int x = 0;
+        int y = 0;
         
-        for(char s : dirs.toCharArray()){
+        for(char c : dirs.toCharArray()){
             int nextX = x;
             int nextY = y;
             
-            switch(s){
+            switch(c){
                 case 'U':
                     nextX += 1;
                     break;
                 case 'D':
                     nextX -= 1;
                     break;
-                case 'R':
-                    nextY += 1;
-                    break;
                 case 'L':
                     nextY -= 1;
                     break;
+                case 'R':
+                    nextY += 1;
+                    break;
             }
             
-            if(nextX > 5 || nextX < -5 || nextY > 5 || nextY < -5){
-                continue;
-            }
+            if(nextX < -5 || nextY < -5 || nextX > 5 || nextY > 5) continue;
             
             String path1 = x + "," + y + "," + nextX + "," + nextY;
             String path2 = nextX + "," + nextY + "," + x + "," + y;
             
             if(!set.contains(path1) && !set.contains(path2)){
                 answer++;
+                
                 set.add(path1);
                 set.add(path2);
             }

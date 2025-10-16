022bfb777c9220312cb951f7ed931e5e5ca972d6
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-10-16 15:04:02 +0900

    [level 2] Title: 주식가격, Time: 76.90 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
+        
         Stack<Integer> stack = new Stack<>();
         
         for(int i = 0; i < prices.length; i++){

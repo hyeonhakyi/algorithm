ffba88121617964504320e19c952e5c8d07595d0
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-10-17 17:20:17 +0900

    [level 2] Title: 스킬트리, Time: 10.36 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -5,18 +5,20 @@ class Solution {
         int answer = 0;
         
         for(int i = 0; i < skill_trees.length; i++){
-            skill_trees[i] = skill_trees[i].replaceAll("[^" +skill+ "]","");
+            skill_trees[i] = skill_trees[i].replaceAll("[^" + skill + "]","");
             boolean check = false;
             for(int j = 0; j < skill_trees[i].length(); j++){
                 if(skill.charAt(j) != skill_trees[i].charAt(j)){
                     check = true;
-                    break;
+                    continue;
                 }
             }
-            if(check == false){
+            if(!check){
                 answer++;
             }
         }
+        
+        
         return answer;
     }
 }

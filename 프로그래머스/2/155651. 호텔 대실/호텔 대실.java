6829b499799f9f6bcbffe95e73b059e756cc2715
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-10-27 16:34:27 +0900

    [level 2] Title: 호텔 대실, Time: 7.88 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,33 +1,31 @@
 import java.util.*;
 
 class Solution {
-    private static final int max_time = 24 * 60 + 10;
     public int solution(String[][] book_time) {
         int answer = 0;
+        int[] time = new int[24 * 60 + 10];
         
-        int[] time = new int[max_time];
-        
-        for(String[] room: book_time){
+        for(String[] room : book_time){
             String start = room[0];
             String end = room[1];
             
-            time[calcul(start)] += 1;
-            time[calcul(end) + 10] -=1; 
+            time[cal(start)] += 1;
+            time[cal(end) + 10] -= 1;
         }
         
         for(int i = 1; i < time.length; i++){
             time[i] += time[i - 1];
-            answer = Math.max(answer, time[i]);
+            answer = Math.max(answer,time[i]);
         }
         
         return answer;
     }
     
-    public int calcul(String time){
+    public static int cal(String time){
         String[] t = time.split(":");
-        String h = t[0];
-        String m = t[1];
+        int h = Integer.parseInt(t[0]);
+        int m = Integer.parseInt(t[1]);
         
-        return ((Integer.parseInt(h) * 60) + Integer.parseInt(m));
+        return (h * 60) + m;
     }
 }

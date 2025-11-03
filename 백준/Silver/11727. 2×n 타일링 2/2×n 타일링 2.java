f7a21646b14c0882f6e3565d765d2684953030d2
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-11-03 16:15:22 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,19 +1,20 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[1001];
-        arr[1] = 1;
-        arr[2] = 3;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         int n = Integer.parseInt(br.readLine());
+         int[] dp = new int[1001];
 
-        for(int i = 3; i <= n; i++){
-            arr[i] = (arr[i-1] + 2 * arr[i-2])%10007;
-        }
-        System.out.println(arr[n]);
+         dp[1] = 1;
+         dp[2] = 3;
 
-    }
-}
+         for(int i = 3; i <= n; i++) {
+             dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+         }
+
+        System.out.println(dp[n]);
+    }//main end
+}//class end

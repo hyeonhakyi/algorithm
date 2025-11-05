7125dd2e50b1589ec58722b7bb00165c68f198d9
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-11-05 15:43:10 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.06 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -3,21 +3,26 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, long k) {
         int[] answer = new int[n];
-        List<Integer> list = new ArrayList<>();
-        long total = 1;
         
+        long[] fact = new long[n + 1];
+        fact[0] = 1;
+        for(int i = 1; i <= n; i++){
+            fact[i] = fact[i - 1] * i;
+        }
+        
+        List<Integer> list = new ArrayList<>();
         for(int i = 1; i <= n; i++){
             list.add(i);
-            total *= i;
         }
         
         k--;
-        int idx = 0;
-        while(idx < n){
-            total /= n - idx;
-            answer[idx++] = list.remove((int) (k/total));
-            k %= total;
+        for(int i = 0; i < n; i++){
+            long block = fact[n - 1 - i];
+            int idx = (int)(k / block);
+            answer[i] = list.remove(idx);
+            k %= block;
         }
+        
         return answer;
     }
 }

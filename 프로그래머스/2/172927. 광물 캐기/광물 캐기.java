06017506480c3de9d6761b87a84caa8f830ec634
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-11-20 17:08:45 +0900

    [level 2] Title: 광물 캐기, Time: 0.92 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,58 +1,60 @@
 import java.util.*;
 
 class Solution {
-    public static void cost(int[] costs, int a, int b, int c){ // 피로도 계산
-        costs[0] += a;
-        costs[1] += b;
-        costs[2] += c;
-    }
-    
     public int solution(int[] picks, String[] minerals) {
-        int answer = 0;
-        int sum = picks[0] + picks[1] + picks[2]; // 곡괭이 갯수
-        int[][] p = new int[sum][3]; 
+        int pick = picks[0] + picks[1] + picks[2];
+        int total = Math.min(minerals.length,pick * 5);
         
-        for (int i = 0 ; i < minerals.length; i += 5){
-            int n = i/5;
-            if(n == sum){
-                break;
+        List<int[]> list = new ArrayList<>();
+        int d = 0;
+        int i = 0;
+        int s = 0;
+        int cnt = 0;
+        for(int idx = 0; idx < total; idx++){
+            String m = minerals[idx];
+            
+            if("diamond".equals(m)){
+                d++;
+            }else if("iron".equals(m)){
+                i++;
+            }else{
+                s++;
             }
-            int[] costs = new int[3];
-            for (int j = i ; j < i+5; j++){
-                if (j == minerals.length){ // 마지막에 5개가 안되는 경우
-                    break;
-                }
-                String x = minerals[j];
-                if (x.equals("diamond")){
-                    cost(costs,1,5,25);
-                }
-                else if (x.equals("iron")){
-                    cost(costs,1,1,5);
-                }
-                else {
-                    cost(costs,1,1,1);
-                }
+            cnt++;
+            
+            if(cnt == 5){
+                list.add(new int[]{d,i,s});
+                d = i = s = 0;
+                cnt = 0;
             }
-            for (int l = 0 ; l < 3; l++){
-                p[n][l] = costs[l];
-            }
-            costs = new int[3];
         }
         
-        Arrays.sort(p, (a1, a2) -> (a2[2]-a1[2]));
+        if(cnt > 0){
+            list.add(new int[]{d,s,i});
+        }
         
-        for(int i = 0; i < sum; i++){
-            if(picks[0] > 0){
-                answer += p[i][0];
-                picks[0]--;
-            }
-            else if(picks[1] > 0){
-                answer += p[i][1];
-                picks[1]--;
-            }
-            else if(picks[2] > 0){
-                answer += p[i][2];
-                picks[2]--;
+        list.sort((a,b) -> {
+            int A = a[0] * 25 + a[1] * 5 + a[2];
+            int B = b[0] * 25 + b[1] * 5 + b[2];
+            return Integer.compare(B,A);
+        });
+        
+            
+        int dia = picks[0];
+        int iro = picks[1];
+        int sto = picks[2];
+        
+        int answer = 0;
+        for(int[] a : list){
+            if(dia > 0){
+                dia--;
+                answer += a[0] + a[1] + a[2];
+            }else if(iro > 0){
+                iro--;
+                answer += a[0]*5 + a[1] + a[2];
+            }else if(sto > 0){
+                sto--;
+                answer += a[0]*25 + a[1]*5 + a[2];
             }
         }
         

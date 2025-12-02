a43bf65302f639571f20d8a34db836d4ce897c5a
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-12-02 15:16:42 +0900

    [Silver I] Title: 스티커, Time: 652 ms, Memory: 122168 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -3,35 +3,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int t = Integer.parseInt(br.readLine());
-		
-		String[] str;
-		
-		for(int tc = 0; tc < t; tc++) {
-			int n = Integer.parseInt(br.readLine());
-			int[][] arr = new int[n+1][2];
-			int[][] dp = new int[n+1][2];
-			
-			for(int i = 0; i < 2; i++) {
-				str = br.readLine().split(" ");
-				for(int j = 1; j <= n; j++) {
-					arr[j][i] = Integer.parseInt(str[j-1]);
-				}
-			}
-			
-			dp[1][0] = arr[1][0];
-			dp[1][1] = arr[1][1];
-			
-			for(int i = 2; i <= n; i++) {
-				dp[i][0] = Math.max(dp[i-1][1], dp[i-2][1]) + arr[i][0];
-				dp[i][1] = Math.max(dp[i-1][0], dp[i-2][0]) + arr[i][1];
-			}
-			System.out.println(Math.max(dp[n][1], dp[n][0]));
-		}//testCase end
-	}//main end
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[][] arr = new int[2][n+1];
+            int[][] dp = new int[2][n+1];
+
+            for (int i = 0; i < 2; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 1; j <= n; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            dp[0][1] = arr[0][1];
+            dp[1][1] = arr[1][1];
+
+            for(int i = 2; i <= n; i++) {
+                dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + arr[0][i];
+                dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + arr[1][i];
+            }
+
+            System.out.println(Math.max(dp[1][n],dp[0][n]));
+        }//test end
+    }//main end
 }//class end

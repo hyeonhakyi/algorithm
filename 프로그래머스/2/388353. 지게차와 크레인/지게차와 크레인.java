ac380982f7dba71b79ad562fb08e487089b7b995
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-12-03 17:09:53 +0900

    [level 2] Title: 지게차와 크레인, Time: 40.64 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -1,71 +1,93 @@
 import java.util.*;
 
+class Node{
+    int x;
+    int y;
+    
+    public Node(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
 class Solution {
-    static int n,m;
+    static int n,m,answer;
+    static int[][] arr;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
     public int solution(String[] storage, String[] requests) {
         n = storage.length;
         m = storage[0].length();
-        int container = n*m;
-        int[] dx = {1,0,-1,0};
-        int[] dy = {0,1,0,-1};
+        arr = new int[n + 2][m + 2];
+        answer = n*m;
         
-        int[][] map = new int[n+2][m+2];
-        for(int[] arr : map){
-            Arrays.fill(arr,-1);
+        for(int[] map : arr){
+            Arrays.fill(map,-1);
         }
         
         for(int i = 0; i < n; i++){
+            String str = storage[i];
             for(int j = 0; j < m; j++){
-                char x = storage[i].charAt(j);
-                map[i+1][j+1] = x - 'A';
+                char chr = str.charAt(j);
+                arr[i + 1][j + 1] = chr - 'A';
             }
         }
         
-        for(String command: requests){
-            int target = command.charAt(0) - 'A';
-            if(command.length() == 2){
-                for(int i = 1; i < n + 1; i++){
-                    for(int j = 1; j < m+1; j++){
-                        if(map[i][j] == target){
-                            map[i][j] = -1;
-                            container--;
-                        }
-                    }
-                }
+        for(String commend : requests){
+            int target = commend.charAt(0) - 'A';
+            
+            if(commend.length() == 2){
+                allChange(target);
             }else{
-                boolean[][] visited = new boolean[n+2][m+2];
-                ArrayDeque<int[]> q = new ArrayDeque<>();
-                visited[0][0] = true;
-                q.add(new int[]{0,0});
-                
-                while(!q.isEmpty()){
-                    int[] cur = q.poll();
-                    int x = cur[0];
-                    int y = cur[1];
-                    
-                    for(int i = 0; i < 4; i++){
-                        int mx = x + dx[i];
-                        int my = y + dy[i];
-                        
-                        if(canGo(mx,my) && !visited[mx][my] && (map[mx][my] == target || map[mx][my] == -1)){
-                            if(map[mx][my] == target){
-                                container--;
-                                map[mx][my] = -1;
-                                visited[mx][my] = true;
-                                continue;
-                            }
-                            q.offer(new int[]{mx,my});
-                            visited[mx][my] = true;
-                        }
-                    }
-                }
+                bfs(target);
             }
         }
         
-        return container;
-    }
+        return answer;
+    }//main end
     
-    static boolean canGo(int x,int y){
-        return x >= 0 && y >= 0 && x < n + 2 && y < m +2;
+    private static void allChange(int target){
+        for(int i = 1; i < n + 1; i++){
+            for(int j = 1; j < m + 1; j++){
+                if(arr[i][j] == target){
+                    arr[i][j] = -1;
+                    answer--;
+                }
+            }
+        }
+    }//allChange end
+    
+    private static void bfs(int target){
+        Queue<Node> q = new LinkedList<>();
+        boolean[][] visited = new boolean[n + 2][m + 2];
+        q.offer(new Node(0,0));
+        visited[0][0] = true;
+        
+        while(!q.isEmpty()){
+            Node now = q.poll();
+            
+            for(int d = 0; d < 4; d++){
+                int nx = now.x + dx[d];
+                int ny = now.y + dy[d];
+                
+                if(!check(nx,ny)) continue;
+                if(visited[nx][ny]) continue;
+                if(arr[nx][ny] == target || arr[nx][ny] == -1){
+                    if(arr[nx][ny] == target){
+                        arr[nx][ny] = -1;
+                        answer--;
+                        visited[nx][ny] = true;
+                        continue;
+                    }
+                    
+                    q.offer(new Node(nx,ny));
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+    }//bfs end
+    
+    private static boolean check(int x,int y){
+        return x >= 0 && y >= 0 && x < n + 2 && y < m + 2;
     }
-}
+}//class end

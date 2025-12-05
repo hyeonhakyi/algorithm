a694aac6403a0169f595ecaa625ae5c86073e43f
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-12-05 16:11:45 +0900

    [level 2] Title: 과제 진행하기, Time: 6.88 ms, Memory: 94.2 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -1,69 +1,69 @@
 import java.util.*;
 
-class Solution {
+class Node{
+    String name;
+    int startTime;
+    int playTime;
     
-    static class Task{
-        String name;
-        int startTime;
-        int playTime;
-        
-        public Task(String name, String start, String playtime){
-            this.name = name;
-            this.startTime = convertTime(start);
-            this.playTime = Integer.parseInt(playtime);
-        }
-        
-        private int convertTime(String time){
-            String[] parts = time.split(":");
-            return Integer.parseInt(parts[0]) * 60 + Integer.parseInt(parts[1]); 
-        }
+    public Node(String name,String start,String playTime){
+        this.name = name;
+        this.startTime = convertTime(start);
+        this.playTime = Integer.parseInt(playTime);
     }
+    
+    private static int convertTime(String time){
+        String[] parts = time.split(":");
+        return Integer.parseInt(parts[0]) * 60 + Integer.parseInt(parts[1]);
+    }
+}
+
+class Solution {
     public String[] solution(String[][] plans) {
-        List<String> result = new ArrayList<>();
+        List<String> answer = new ArrayList<>();
         
-        List<Task> tasks = new ArrayList<>();
+        List<Node> list = new ArrayList<>();
+        
         for(String[] plan : plans){
-            tasks.add(new Task(plan[0],plan[1],plan[2]));
+            list.add(new Node(plan[0],plan[1],plan[2]));
         }
         
-        tasks.sort(Comparator.comparingInt(o -> o.startTime));
+        list.sort(Comparator.comparingInt(o -> o.startTime));
         
-        Stack<Task> pausedTasks = new Stack<>();
+        Stack<Node> stack = new Stack<>();
         
-        for(int i = 0; i < tasks.size() - 1; i++){
-            Task current = tasks.get(i);
-            Task next = tasks.get(i + 1);
+        for(int i = 0; i < list.size() - 1; i++){
+            Node current = list.get(i);
+            Node next = list.get(i+1);
             
-            int currentEndTime = current.startTime + current.playTime;
-            
-            if(currentEndTime <= next.startTime){
-                result.add(current.name);
-                int freeTime = next.startTime - currentEndTime;
+            int currentTotalTime = current.startTime + current.playTime;
+            if(next.startTime >= currentTotalTime){
+                answer.add(current.name);
+                int freeTime = next.startTime - currentTotalTime;
                 
-                while(freeTime > 0 &&  !pausedTasks.isEmpty()){
-                    Task paused = pausedTasks.pop();
+                while(freeTime > 0 && !stack.isEmpty()){
+                    Node paused = stack.pop();
                     
                     if(freeTime >= paused.playTime){
                         freeTime -= paused.playTime;
-                        result.add(paused.name);
+                        answer.add(paused.name);
                     }else{
                         paused.playTime -= freeTime;
-                        pausedTasks.push(paused);
+                        stack.push(paused);
                         break;
                     }
                 }
             }else{
                 current.playTime -= next.startTime - current.startTime;
-                pausedTasks.push(current);
+                stack.push(current);
             }
         }
         
-        result.add(tasks.get(tasks.size() - 1).name);
+        answer.add(list.get(list.size() - 1).name);
         
-        while(!pausedTasks.isEmpty()){
-            result.add(pausedTasks.pop().name);
+        while(!stack.isEmpty()){
+            answer.add(stack.pop().name);
         }
         
-        return result.toArray(new String[0]);
+        return answer.toArray(new String[0]);
     }
 }

c3483b64ca2e98376a16b9b5c97624c0259e7502
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2025-12-10 16:27:38 +0900

    [level 2] Title: 요격 시스템, Time: 432.81 ms, Memory: 175 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -2,21 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] targets) {
-        int answer = 0;    
-        
-        Arrays.sort(targets, (o1,o2) -> {
-            if(o1[1] == o2[1])
-                return o1[0] - o2[0];
-            return o1[1] - o2[1];
-        });
-        
-        int end = targets[0][1];
-        answer++;
+        int answer = 0;
+        Arrays.sort(targets, (a,b) -> Integer.compare(a[1],b[1]));
+        int last = Integer.MIN_VALUE;
         
         for(int[] t : targets){
-            if(t[0] >= end){
-                end = t[1];
+            int s = t[0];
+            int e = t[1];
+            
+            if(last <= s){
                 answer++;
+                last = e;
             }
         }
         

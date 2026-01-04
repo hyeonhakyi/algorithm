7ab1d2733b2412ec4b6093c53ec03ca273803dc8
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-01-04 16:35:15 +0900

    [level 3] Title: 단속카메라, Time: 10.37 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -3,13 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
         int answer = 0;
+        
         Arrays.sort(routes, (a,b) -> Integer.compare(a[1],b[1]));
         
-        int preCam = Integer.MIN_VALUE;
-        for(int[] r : routes){
-            if(r[0] > preCam){
-                preCam = r[1];
+        int prev = Integer.MIN_VALUE;
+        for(int[] route : routes){
+            int now = route[0];
+            
+            if(prev < now){
                 answer++;
+                prev = route[1];
             }
         }
         

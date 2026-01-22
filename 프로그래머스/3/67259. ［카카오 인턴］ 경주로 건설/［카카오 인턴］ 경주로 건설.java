b8a9168f771e158dca9cbd0e76c6a6f2e58d86a6
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-01-22 15:57:07 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 2041.49 ms, Memory: 714 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -3,32 +3,29 @@ import java.util.*;
 class Node{
     int x;
     int y;
+    int money;
     int dir;
-    int value;
-    public Node(int x,int y,int dir,int value){
+    public Node(int x,int y,int money,int dir){
         this.x = x;
         this.y = y;
+        this.money = money;
         this.dir = dir;
-        this.value = value;
     }
 }
 
 class Solution {
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
-    static int n,m,answer;
+    static int n;
+    static int answer = Integer.MAX_VALUE;
+    static int[] dx = {-1,0,1,0};
+    static int[] dy = {0,-1,0,1};
     static int[][][] arr;
-    static boolean[][] visited;
     public int solution(int[][] board) {
-        answer = Integer.MAX_VALUE;
         n = board.length;
-        m = board.length;
-        visited = new boolean[n][m];
-        arr = new int[4][n][m];
+        arr = new int[4][n][n];
         
         for(int x = 0; x < 4; x++){
             for(int i = 0; i < n; i++){
-                for(int j = 0; j < m; j++){
+                for(int j = 0; j < n; j++){
                     arr[x][i][j] = Integer.MAX_VALUE;
                 }
             }
@@ -37,49 +34,47 @@ class Solution {
         bfs(board);
         
         return answer;
-    }
+    }//main end
     
-    public void bfs(int[][] board){
+    private static void bfs(int[][] board){
         Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(0,0,-1,0));
+        q.offer(new Node(0,0,0,-1));
+        boolean[][] visited = new boolean[n][n];
         visited[0][0] = true;
         
         while(!q.isEmpty()){
             Node now = q.poll();
-            int nowX = now.x;
-            int nowY = now.y;
-            int nowDir = now.dir;
-            int nowValue = now.value;
             
-            if(nowX == n - 1 && nowY == m - 1){
-                answer = Math.min(answer,nowValue);
+            if(now.x == n - 1 && now.y == n - 1){
+                answer = Math.min(answer,now.money);
             }
             
             for(int d = 0; d < 4; d++){
-                int nx = nowX + dx[d];
-                int ny = nowY + dy[d];
-                int nextValue = nowValue;
+                int nx = now.x + dx[d];
+                int ny = now.y + dy[d];
+                int nextMoney = now.money;
                 
-                if(nowDir == -1){
-                    nextValue += 100;
-                }else if(nowDir == d){
-                    nextValue += 100;
+                if(now.dir == -1){
+                    nextMoney += 100;
+                }else if(now.dir == d){
+                    nextMoney += 100;
                 }else{
-                    nextValue += 600;
+                    nextMoney += 600;
                 }
                 
                 if(!check(nx,ny)) continue;
                 if(board[nx][ny] == 1) continue;
-                if(!visited[nx][ny] || arr[d][nx][ny] >= nextValue){
+                
+                if(!visited[nx][ny] || arr[d][nx][ny] >= nextMoney){
+                    arr[d][nx][ny] = nextMoney;
                     visited[nx][ny] = true;
-                    arr[d][nx][ny] = nextValue;
-                    q.offer(new Node(nx,ny,d,nextValue));  
-                } 
+                    q.offer(new Node(nx,ny,nextMoney,d));
+                }
             }
         }
-    }
+    }//bfs end
     
-    public boolean check(int x,int y){
-        return x >= 0 && x < n && y >= 0 && y < m;
-    }
-}
+    private static boolean check(int x,int y){
+        return x >= 0 && x < n && y >= 0 && y < n;
+    }//check end
+}//class end

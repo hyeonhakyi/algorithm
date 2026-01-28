6878cfe8d5d00790201bd7fd81a04e566315e921
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-01-28 16:22:30 +0900

    [level 3] Title: 거스름돈, Time: 68.56 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -1,15 +1,16 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int[] money) {
-        int[] dp = new int[n+1];
-        int mod = 1_000_000_007;
+        int[] dp = new int[n + 1];
         dp[0] = 1;
+        int mod = 1_000_000_007;
         
-        for(int m : money){
-            for(int i = m; i <= n; i++){
-                dp[i] = (dp[i] + dp[i - m]) % mod;
+        for(int coin : money){
+            for(int i = coin; i <= n; i++){
+                dp[i] = (dp[i] + dp[i - coin]) % mod;
             }
         }
-        
         
         return dp[n];
     }

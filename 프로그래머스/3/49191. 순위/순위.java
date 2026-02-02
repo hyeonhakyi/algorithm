91cd601a3b2db4309522bf5255cb869d94e7e705
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-02-02 16:45:53 +0900

    [level 3] Title: 순위, Time: 16.66 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -6,17 +6,17 @@ class Solution {
         int[][] check = new int[n+1][n+1];
         
         for(int[] r : results){
-            int win = r[0];
-            int lose = r[1];
-            check[win][lose] = 1;
-            check[lose][win] = -1;
+            int a = r[0];
+            int b = r[1];
+            
+            check[a][b] = 1;
+            check[b][a] = -1;
         }
-        
         
         for(int b = 1; b <= n; b++){
             for(int a = 1; a <= n; a++){
                 for(int c = 1; c <= n; c++){
-                    if(check[a][b] == 1 && check[b][c] == 1){
+                    if(check[a][c] == 1 && check[b][c] == 1){
                         check[a][c] = 1;
                         check[c][a] = -1;
                     }else if(check[a][b] == -1 && check[b][c] == -1){
@@ -27,15 +27,17 @@ class Solution {
             }
         }
         
+        
         for(int i = 1; i <= n; i++){
-            int cnt = 0;
+            int count = 0;
             for(int j = 1; j <= n; j++){
                 if(i == j) continue;
-                if(check[i][j] != 0) cnt++;
+                if(check[i][j] != 0) count++;
             }
-            if(cnt == n - 1) answer++;
+            
+            if(count == (n - 1)) answer++;
         }
         
         return answer;
-    }
-}
+    }//main end
+}//class end

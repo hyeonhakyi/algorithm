52abbbcf266a41bdcb820d38e1ab1222a6ed9f8d
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-02-04 16:27:59 +0900

    [level 3] Title: 양과 늑대, Time: 3.84 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -6,26 +6,30 @@ class Solution {
     static int maxSheep = 0;
     public int solution(int[] info, int[][] edges) {
         Solution.info = info;
-        int n = info.length;
         
-        list = new ArrayList[n];
-        for(int i = 0; i < n; i++){
+        list = new ArrayList[info.length];
+        
+        for(int i = 0; i < info.length; i++){
             list[i] = new ArrayList<>();
         }
         
-        for(int[] i : edges){
-            list[i[0]].add(i[1]);
+        for(int[] e : edges){
+            int s = e[0];
+            int en = e[1];
+            
+            list[s].add(en);
         }
         
-        List<Integer> candidate = new ArrayList<>();
-        candidate.add(0);
-        dfs(0,0,candidate);
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        
+        dfs(0,0,list);
         
         return maxSheep;
-    }
+    }//main end
     
-    public static void dfs(int wolf,int sheep,List<Integer> can){
-        maxSheep = Math.max(sheep,maxSheep);
+    private static void dfs(int sheep,int wolf,List<Integer> can){
+        maxSheep = Math.max(maxSheep,sheep);
         
         for(int i = 0; i < can.size(); i++){
             int next = can.get(i);
@@ -34,18 +38,17 @@ class Solution {
             int newWolf = wolf;
             
             if(info[next] == 0){
-                newSheep++;   
+                newSheep++;
             }else{
                 newWolf++;
             }
             
             if(newWolf >= newSheep) continue;
-            
             List<Integer> newCan = new ArrayList<>(can);
             newCan.remove(i);
             newCan.addAll(list[next]);
             
-            dfs(newWolf,newSheep,newCan);
+            dfs(newSheep,newWolf,newCan);
         }
     }//dfs end
-}
+}//class end

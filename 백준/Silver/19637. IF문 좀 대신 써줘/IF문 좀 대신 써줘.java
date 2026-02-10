3bf19a965431d2b0293cc225ded1fd1b29fb9a76
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-02-10 16:45:53 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 308 ms, Memory: 60600 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.java
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.java
@@ -6,7 +6,6 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
@@ -14,35 +13,37 @@ public class Main {
         String[] title = new String[n];
         int[] numbers = new int[n];
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
-            title[i] = (st.nextToken());    
+            title[i] = st.nextToken();
             numbers[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i = 0; i < m; i++) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < m; i++){
             int num = Integer.parseInt(br.readLine());
-            int idx = binarySearch(numbers, num);
-            sb.append(title[idx]).append("\n");
+            String str = title[binarySearch(numbers, num)];
+            sb.append(str).append("\n");
         }
 
         System.out.println(sb.toString());
     }//main end
 
-    private static int binarySearch(int[] numbers, int num) {
+    private static int binarySearch(int[] numbers,int num){
         int left = 0;
         int right = numbers.length - 1;
-        int result = 0;
-
-        while(left <= right) {
+        int target = 0;
+        while(left <= right){
             int mid = (left + right) / 2;
+
             if(num <= numbers[mid]){
-                result = mid;
+                target = mid;
                 right = mid - 1;
             }else{
                 left = mid + 1;
             }
         }
-        return result;
+
+        return target;
     }//binarySearch end
 }//class end

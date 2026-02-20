e5c619db67abdfd68d27b25c325d43dcdedab84c
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-02-20 15:04:36 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -3,9 +3,7 @@ select
     i.NAME
 from
     ANIMAL_INS as i
-join
+right join
     ANIMAL_OUTS as o on i.ANIMAL_ID = o.ANIMAL_ID
 order by
-    datediff(i.DATETIME,o.DATETIME)
-limit 
-    2
+    datediff(o.DATETIME, i.DATETIME) desc limit 2

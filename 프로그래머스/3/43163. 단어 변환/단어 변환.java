35e38932d8482c1e4fa743ba0600cc8741a15afd
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-02-24 15:57:57 +0900

    [level 3] Title: 단어 변환, Time: 0.44 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,52 +1,63 @@
+
 import java.util.*;
 
 class Word{
     String word;
-    int count;
-    public Word(String word,int count){
+    int dist;
+    public Word(String word,int dist){
         this.word = word;
-        this.count = count;
+        this.dist = dist;
     }
 }
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        if(!Arrays.asList(words).contains(target)) return 0;
-        Queue<Word> q = new LinkedList<>();
-        boolean[] visited = new boolean[words.length];
+        boolean check = false;
+        for(String w : words){
+            if(w.equals(target)){
+                check = true;
+                break;
+            }
+        }
         
+        if(!check){
+            return 0;
+        }
+        
+        boolean[] visited = new boolean[words.length];
+        Queue<Word> q = new LinkedList<>();
         q.offer(new Word(begin,0));
         
         while(!q.isEmpty()){
             Word now = q.poll();
             
             if(now.word.equals(target)){
-                return now.count;
+                return now.dist;
             }
             
             for(int i = 0; i < words.length; i++){
-                if(!visited[i] && canCov(words[i],now.word)){
-                    q.offer(new Word(words[i],now.count + 1));
+                if(visited[i]) continue;
+                if(diffCheck(now.word,words[i])){
+                    q.offer(new Word(words[i],now.dist + 1));
                     visited[i] = true;
                 }
             }
         }
         
         return 0;
-    }//main end
+    }//solution end
     
-    private static boolean canCov(String word,String target){
-        int diff = 0;
-        for(int i = 0; i < word.length(); i++){
-            if(word.charAt(i) != target.charAt(i)){
-                diff++;
+    private static boolean diffCheck(String a,String b){
+        int count = 0;
+        for(int i = 0; i < a.length(); i++){
+            if(a.charAt(i) != b.charAt(i)){
+                count++;
             }
             
-            if(diff > 1){
+            if(count > 1){
                 return false;
             }
         }
-        return diff == 1;
-    }//canCov end
-}
+        return true;
+    }//diffCheck end
+}//class end

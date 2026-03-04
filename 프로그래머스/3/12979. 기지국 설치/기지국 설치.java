99d567ebee52f0f2eb2890bc85c5ac3f0610b831
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-03-04 14:55:14 +0900

    [level 3] Title: 기지국 설치, Time: 1.05 ms, Memory: 56.1 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -3,9 +3,9 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        int now = 1;
         int cover = 2 * w + 1;
         int idx = 0;
+        int now = 1;
         
         while(now <= n){
             if(idx < stations.length && now >= stations[idx] - w){
@@ -16,7 +16,7 @@ class Solution {
                 answer++;
             }
         }
-        
+
         return answer;
-    }
-}
+    }//solution end
+}//class end

f789dcbe9b555c5e093770bf1bea582f14a90e4a
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-03-13 15:03:06 +0900

    [level 3] Title: 가장 먼 노드, Time: 35.08 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,52 +1,57 @@
 import java.util.*;
 
 class Solution {
+    static int[] dist;
+    static boolean[] visited;
+    static List<Integer>[] list;
     public int solution(int n, int[][] edge) {
         int answer = 0;
         
-        List<Integer>[] list = new ArrayList[n + 1];
-        
+        list = new ArrayList[n + 1];
         for(int i = 1; i <= n; i++){
             list[i] = new ArrayList<>();
         }
         
+        dist = new int[n + 1];
+        Arrays.fill(dist,-1);
+        dist[1] = 1;
         
-        for(int[] e : edge){
-            int v = e[0];
-            int t = e[1];
-            
-            list[v].add(t);
-            list[t].add(v);
+        visited = new boolean[n + 1];
+        visited[1] = true;
+        
+        for(int[] i : edge){
+            int s = i[0];
+            int e = i[1];
+            list[s].add(e);
+            list[e].add(s);
         }
         
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist,-1);
         Queue<Integer> q = new LinkedList<>();
         q.offer(1);
-        dist[1] = 0;
         
         while(!q.isEmpty()){
             int now = q.poll();
             
             for(int next : list[now]){
-                if(dist[next] == -1){
+                if(!visited[next]){
                     dist[next] = dist[now] + 1;
                     q.offer(next);
+                    visited[next] = true;
                 }
             }
         }
         
-        int max = 0;
-        for(int i = 1; i <= n; i++){
-            max = Math.max(dist[i],max);
+        int max = Integer.MIN_VALUE;
+        for(int i : dist){
+            max = Math.max(max,i);
         }
         
-        for(int i = 1; i <= n; i++){
-            if(max == dist[i]){
+        for(int i : dist){
+            if(i == max){
                 answer++;
             }
         }
         
         return answer;
-    }//main end
+    }//solution end
 }//class end

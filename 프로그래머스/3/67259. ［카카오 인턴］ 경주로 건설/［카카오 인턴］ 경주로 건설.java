71c94572145845e33f45f0e1d778f0e942e507e4
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-03-23 14:57:12 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 9.63 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -3,78 +3,82 @@ import java.util.*;
 class Node{
     int x;
     int y;
-    int money;
-    int dir;
-    public Node(int x,int y,int money,int dir){
+    int weight;
+    int dist;
+    public Node(int x,int y,int weight,int dist){
         this.x = x;
         this.y = y;
-        this.money = money;
-        this.dir = dir;
+        this.weight = weight;
+        this.dist = dist;
     }
 }
 
 class Solution {
     static int n;
-    static int answer = Integer.MAX_VALUE;
-    static int[] dx = {-1,0,1,0};
-    static int[] dy = {0,-1,0,1};
-    static int[][][] arr;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
     public int solution(int[][] board) {
+        int answer = 0;
         n = board.length;
-        arr = new int[4][n][n];
         
-        for(int x = 0; x < 4; x++){
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    arr[x][i][j] = Integer.MAX_VALUE;
-                }
-            }
-        }
-        
-        bfs(board);
+        answer = bfs(board);
         
         return answer;
-    }//main end
+    }//solution end
     
-    private static void bfs(int[][] board){
+    private static int bfs(int[][] arr){
         Queue<Node> q = new LinkedList<>();
         q.offer(new Node(0,0,0,-1));
-        boolean[][] visited = new boolean[n][n];
-        visited[0][0] = true;
+        
+        int[][][] cost = new int[n][n][4];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                Arrays.fill(cost[i][j], Integer.MAX_VALUE);
+            }
+        }
+
         
         while(!q.isEmpty()){
-            Node now = q.poll();
-            
-            if(now.x == n - 1 && now.y == n - 1){
-                answer = Math.min(answer,now.money);
-            }
+            Node cur = q.poll();
+            int nowX = cur.x;
+            int nowY = cur.y;
             
             for(int d = 0; d < 4; d++){
-                int nx = now.x + dx[d];
-                int ny = now.y + dy[d];
-                int nextMoney = now.money;
-                
-                if(now.dir == -1){
-                    nextMoney += 100;
-                }else if(now.dir == d){
-                    nextMoney += 100;
-                }else{
-                    nextMoney += 600;
-                }
+                int nx = nowX + dx[d];
+                int ny = nowY + dy[d];
+                int nextWeight;
                 
                 if(!check(nx,ny)) continue;
-                if(board[nx][ny] == 1) continue;
+                if(arr[nx][ny] == 1) continue;
                 
-                if(!visited[nx][ny] || arr[d][nx][ny] >= nextMoney){
-                    arr[d][nx][ny] = nextMoney;
-                    visited[nx][ny] = true;
-                    q.offer(new Node(nx,ny,nextMoney,d));
+                // 수정: 첫 이동은 무조건 100
+                if(cur.dist == -1){
+                    nextWeight = cur.weight + 100;
+                }
+                // 수정: 같은 방향이면 100
+                else if(cur.dist == d){
+                    nextWeight = cur.weight + 100;
+                }
+                // 수정: 방향 전환이면 600
+                else{
+                    nextWeight = cur.weight + 600;
+                }
+                if(cost[nx][ny][d] > nextWeight){
+                    cost[nx][ny][d] = nextWeight;
+                    q.offer(new Node(nx,ny,nextWeight,d));
                 }
             }
         }
+        
+        int result = Integer.MAX_VALUE;
+        for(int d = 0; d < 4; d++){
+            result = Math.min(result, cost[n - 1][n - 1][d]);
+        }
+
+        return result;    
     }//bfs end
     
     private static boolean check(int x,int y){
         return x >= 0 && x < n && y >= 0 && y < n;
-    }//check end
+    }
 }//class end

03e19bbf74230bc86a8b394b4022d9105d57a753
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-04-04 14:56:45 +0900

    [level 3] Title: 인사고과, Time: 131.54 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -2,39 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] scores) {
-        int[] wan = scores[0];
-        int wanSum = wan[0] + wan[1];
+        int wanFirst = scores[0][0];
+        int wanSecond = scores[0][1];
+        int wanSum = wanFirst + wanSecond;
         
-        Arrays.sort(scores, (a,b) ->{
-            if(a[0] != b[0]){
-                return b[0] - a[0];
-            }else{
-                return a[1] - b[1];
+        Arrays.sort(scores, (a, b) -> {
+            if (a[0] == b[0]) {
+                return Integer.compare(a[1], b[1]);
             }
+            return Integer.compare(b[0], a[0]);
         });
         
-        int maxB = 0;
-        List<Integer> list = new ArrayList<>();
-        for(int[] s : scores){
-            int a = s[0];
-            int b = s[1];
-            
-            if(wan[0] < a && wan[1] < b){
-                return -1;
-            }
-            
-            if(b >= maxB){
-                list.add(a + b);
-                maxB = b;
-            }
-        }
-        
+        int maxSecond = Integer.MIN_VALUE;
         int answer = 1;
-        for(int i : list){
-            if(i > wanSum){
+        for(int[] score : scores){
+            int fir = score[0];
+            int sec = score[1];
+            
+            if(sec < maxSecond){
+                if(fir == wanFirst && sec == wanSecond){
+                    return -1;
+                }
+                continue;
+            }
+            
+            maxSecond = Math.max(maxSecond,sec);
+            
+            int sum = fir + sec;
+            if(wanSum < sum){
                 answer++;
             }
-        }        
+        }    
+        
         return answer;
-    }
-}
+    }//solution end
+}//class end

5d94e525097afab769aedc4cf0a7328eb2b9ec99
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-04-15 14:53:32 +0900

    [Silver II] Title: 에디터, Time: 604 ms, Memory: 141616 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,52 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.ListIterator;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
+
         int n = Integer.parseInt(br.readLine());
 
-        LinkedList<Character> list = new LinkedList<>();
-        for (char c : str.toCharArray()) {
-            list.add(c);
+        ArrayDeque<Character> left = new ArrayDeque<>();
+        ArrayDeque<Character> right = new ArrayDeque<>();
+
+        for(int i = 0; i < str.length(); i++){
+            left.add(str.charAt(i));
         }
 
-        ListIterator<Character> cursor = list.listIterator(list.size()); // 커서 맨 뒤부터 시작
-
-        for (int i = 0; i < n; i++) {
-            String commandLine = br.readLine();
-            StringTokenizer st = new StringTokenizer(commandLine);
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
             String cmd = st.nextToken();
 
-            switch (cmd) {
-                case "L":
-                    if (cursor.hasPrevious()) cursor.previous();
-                    break;
-                case "D":
-                    if (cursor.hasNext()) cursor.next();
-                    break;
-                case "B":
-                    if (cursor.hasPrevious()) {
-                        cursor.previous();
-                        cursor.remove();
-                    }
-                    break;
-                case "P":
-                    char ch = st.nextToken().charAt(0);
-                    cursor.add(ch);
-                    break;
+            if(cmd.equals("L")){
+                if(!left.isEmpty()){
+                    right.addFirst(left.removeLast());
+                }
+            }else if(cmd.equals("D")){
+                if(!right.isEmpty()){
+                    left.addLast(right.removeFirst());
+                }
+            }else if(cmd.equals("B")){
+                if(!left.isEmpty()){
+                    left.removeLast();
+                }
+            }else{
+                char value = st.nextToken().charAt(0);
+                left.addLast(value);
             }
         }
-
         StringBuilder sb = new StringBuilder();
-        for (char c : list) {
-            sb.append(c);
+
+        while(!left.isEmpty()){
+            sb.append(left.removeFirst());
         }
-        System.out.println(sb);
-    }
-}
+
+        while(!right.isEmpty()){
+            sb.append(right.removeFirst());
+        }
+
+        System.out.println(sb.toString());
+    }//main end
+}//class end

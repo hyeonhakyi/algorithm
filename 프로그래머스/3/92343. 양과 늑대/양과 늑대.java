87b667ce9f76ac8a2690a8a937adadc0f894c852
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-04-15 15:18:05 +0900

    [level 3] Title: 양과 늑대, Time: 5.08 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -2,53 +2,58 @@ import java.util.*;
 
 class Solution {
     static List<Integer>[] list;
-    static int[] info;
-    static int maxSheep = 0;
+    static int[] infoArr;
+    static int n,answer;
     public int solution(int[] info, int[][] edges) {
-        Solution.info = info;
+        answer = 0;
+        n = info.length;
+        infoArr = info;
         
-        list = new ArrayList[info.length];
-        
-        for(int i = 0; i < info.length; i++){
+        list = new ArrayList[n];
+        for(int i = 0; i < n; i++){
             list[i] = new ArrayList<>();
         }
-        
-        for(int[] e : edges){
-            int s = e[0];
-            int en = e[1];
             
-            list[s].add(en);
+        for(int[] i : edges){
+            int s = i[0];
+            int e = i[1];
+            
+            list[s].add(e);
         }
         
-        List<Integer> list = new ArrayList<>();
-        list.add(0);
+        List<Integer> nextNode = new ArrayList<>();
+        nextNode.add(0);
+        dfs(0,0,nextNode);
         
-        dfs(0,0,list);
-        
-        return maxSheep;
-    }//main end
+        return answer;
+    }//solution end
     
-    private static void dfs(int sheep,int wolf,List<Integer> can){
-        maxSheep = Math.max(maxSheep,sheep);
+    private static void dfs(int sheep,int wolf,List<Integer> nextNode){
+        answer = Math.max(answer,sheep);
         
-        for(int i = 0; i < can.size(); i++){
-            int next = can.get(i);
+        for(int i = 0; i < nextNode.size(); i++){
+            int current = nextNode.get(i);
             
-            int newSheep = sheep;
-            int newWolf = wolf;
+            int nextSheep = sheep;
+            int nextWolf = wolf;
             
-            if(info[next] == 0){
-                newSheep++;
+            if(infoArr[current] == 0){
+                nextSheep++;
             }else{
-                newWolf++;
+                nextWolf++;
             }
             
-            if(newWolf >= newSheep) continue;
-            List<Integer> newCan = new ArrayList<>(can);
-            newCan.remove(i);
-            newCan.addAll(list[next]);
+            if(nextWolf >= nextSheep) continue;
             
-            dfs(newSheep,newWolf,newCan);
+            List<Integer> next = new ArrayList<>(nextNode);
+            
+            next.remove(i);
+            
+            for(int nextI : list[current]){
+                next.add(nextI);
+            }
+            
+            dfs(nextSheep,nextWolf,next);
         }
     }//dfs end
 }//class end

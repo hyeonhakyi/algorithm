c672923e331603b84130feea11894a139a4994ed
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-04-23 15:11:09 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.77 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,19 +1,20 @@
+import java.util.*;
+
 class Solution {
     public String solution(String s) {
-        StringBuilder answer = new StringBuilder();
-        
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
         
         String[] arr = s.split(" ");
-        for(int i = 0; i < arr.length; i++){
-            String st = arr[i];
-            
-            max = Math.max(Integer.parseInt(st),max);
-            min = Math.min(Integer.parseInt(st),min);
-        }
-        answer.append(min).append(" ").append(max);
         
-        return answer.toString();
-    }
-}
+        for(int i = 0; i < arr.length; i++){
+            max = Math.max(Integer.parseInt(arr[i]),max);
+            min = Math.min(Integer.parseInt(arr[i]),min);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append(min).append(" ").append(max);
+        
+        return sb.toString();
+    }//solution end
+}//class end

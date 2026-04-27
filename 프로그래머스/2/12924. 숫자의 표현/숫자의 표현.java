b0a87945e521ba86f7f22a07607129c272b248e2
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-04-27 13:52:57 +0900

    [level 2] Title: 숫자의 표현, Time: 0.60 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,27 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int sum = 1;
-        int start = 1;
-        int end = 1;
-        
-        while(start <= end){
-            if(sum == n){
-                answer++;
-                sum -= start;
-                start++;
-            }else if(sum < n){
-                end++;
-                if(end > n) break;
-                sum += end;
-            }else{
-                sum -= start;
-                start++;
-            }
-        }
-        
         return answer;
-    }//solution end
-}//class end
+    }
+}

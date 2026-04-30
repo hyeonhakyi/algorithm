7fb624d580aeaee219dc5ba53758343249b62764
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-04-30 15:50:33 +0900

    [level 3] Title: 보행자 천국, Time: 150.16 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
@@ -1,47 +1,42 @@
-import java.util.*;
-
 class Solution {
-    int MOD = 20170805;
+    static final int MOD = 20170805;
+
     public int solution(int m, int n, int[][] cityMap) {
-        int[][] dp = new int[m][n];
-        int[][] right = new int[m][n];
-        int[][] down = new int[m][n];
-        
-        dp[0][0] = 1;
-        
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(cityMap[i][j] == 1){
-                    dp[i][j] = 0;
-                    right[i][j] = 0;
-                    down[i][j] = 0;
+        int[][][] dp = new int[m][n][2];
+
+        // 수정: 시작점은 한 번만 세어야 함
+        dp[0][0][0] = 1;
+        dp[0][0][1] = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (cityMap[i][j] == 1) {
+                    dp[i][j][0] = 0;
+                    dp[i][j][1] = 0;
                     continue;
                 }
-                
-                if(i == 0 && j == 0) continue;
-                
-                if(j > 0){
-                    if(cityMap[i][j - 1] == 2){
-                        right[i][j] = right[i][j -1];
-                    }else{
-                        right[i][j] = dp[i][j - 1];
+
+                // 위에서 아래로 내려와서 현재 칸에 도착하는 경우
+                if (i > 0 && cityMap[i - 1][j] != 1) {
+                    if (cityMap[i - 1][j] == 2) {
+                        dp[i][j][0] = dp[i - 1][j][0];
+                    } else {
+                        dp[i][j][0] = (dp[i - 1][j][0] + dp[i - 1][j][1]) % MOD;
                     }
-                    right[i][j] %= MOD;
                 }
-                
-                if(i > 0){
-                    if(cityMap[i - 1][j] == 2){
-                        down[i][j] = down[i - 1][j];
-                    }else{
-                        down[i][j] = dp[i - 1][j];
+
+                // 왼쪽에서 오른쪽으로 와서 현재 칸에 도착하는 경우
+                if (j > 0 && cityMap[i][j - 1] != 1) {
+                    if (cityMap[i][j - 1] == 2) {
+                        dp[i][j][1] = dp[i][j - 1][1];
+                    } else {
+                        dp[i][j][1] = (dp[i][j - 1][0] + dp[i][j - 1][1]) % MOD;
                     }
-                    down[i][j] %= MOD;
                 }
-                
-                dp[i][j] = (right[i][j] + down[i][j]) % MOD;
             }
         }
-        
-        return dp[m - 1][n - 1];
+
+        return (dp[m - 1][n - 1][0] + dp[m - 1][n - 1][1]) % MOD;
     }
 }

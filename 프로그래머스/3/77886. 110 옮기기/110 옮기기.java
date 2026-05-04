ffb87f74a70bdcf1132552a310b3428479e6840e
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-05-04 14:38:27 +0900

    [level 3] Title: 110 옮기기, Time: 55.99 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
@@ -5,32 +5,49 @@ class Solution {
         String[] answer = new String[s.length];
         
         for(int i = 0; i < s.length; i++){
-            int cnt = 0;
-            StringBuilder sb = new StringBuilder();
-            for(int j = 0; j < s[i].length(); j++){
-                sb.append(s[i].charAt(j));
-                
-                if(sb.length() < 3) continue;
-                
-                if("110".equals(sb.substring(sb.length() - 3))){
-                    sb.setLength(sb.length() - 3);
-                    cnt++;
-                }
-            }
-            
-            int idx = sb.length();
-            while(idx > 0){
-                if(sb.charAt(idx - 1) == '1'){
-                    idx--;
-                }else{
-                    break;
-                }
-            }
-            sb.insert(idx,"110".repeat(cnt));
-            answer[i] = sb.toString();
-            
+            answer[i] = solve(s[i]);
         }
         
         return answer;
-    }
-}//main end
+    }//solution end
+    
+    private static String solve(String str){
+        char[] stack = new char[str.length()];
+        
+        int top = 0;
+        int count110 = 0;
+        
+        for(int i = 0; i < str.length(); i++){
+            stack[top++] = str.charAt(i);
+            
+            if(top >= 3 && stack[top - 3] == '1' && stack[top - 2] == '1' && stack[top - 1] == '0'){
+                top -= 3;
+                count110++;
+            }
+        }
+        
+        int countPos = -1;
+        
+        for(int i = 0; i < top; i++){
+            if(stack[i] == '0'){
+                countPos = i;
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i <= countPos; i++){
+            sb.append(stack[i]);
+        }
+        
+        for(int i = 0; i < count110; i++){
+            sb.append("110");
+        }
+        
+        for(int i = countPos + 1; i < top; i++){
+            sb.append(stack[i]);
+        }
+        
+        return sb.toString();
+    }//solve end
+}//class end

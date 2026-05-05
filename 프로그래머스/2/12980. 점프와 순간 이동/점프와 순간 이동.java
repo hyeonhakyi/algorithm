bb7e0b4e00f4679cfb48fb4dc318381875e24c1c
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-05-05 16:17:33 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,17 +2,19 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int ans = 0;
-        
-        while(n != 0){
-            if(n % 2 == 0){
+        int answer = 0;
+
+        while (n > 0) {
+            if (n % 2 == 0) {
+                // 수정: 짝수면 순간이동으로 온 것이므로 배터리 소모 없이 절반으로 이동
                 n /= 2;
-            }else{
-                ans++;
-                n--;
+            } else {
+                // 수정: 홀수면 점프가 필요하므로 배터리 1 사용
+                n -= 1;
+                answer++;
             }
         }
 
-        return ans;
-    }
-}
+        return answer;
+    }//solution end
+}//class end

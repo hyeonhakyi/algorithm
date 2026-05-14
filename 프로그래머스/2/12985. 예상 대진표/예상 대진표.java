0c0799426dc787fad19e8b44a21a7751a8d2ae11
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-05-14 14:49:22 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,18 +1,15 @@
 import java.util.*;
 
-class Solution
-{
-    public int solution(int n, int a, int b)
-    {
+class Solution{
+    public int solution(int n, int a, int b){
         int answer = 0;
         
         while(a != b){
-            a = a / 2 + a % 2;
-            b = b / 2 + b % 2;
-            
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
             answer++;
         }
         
         return answer;
-    }
-}
+    }//solution end
+}//class end

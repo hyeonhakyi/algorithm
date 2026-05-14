ae9abf2682e49fdf5a859b2cb44b613c76127650
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-05-14 15:22:10 +0900

    [level 2] Title: 프로세스, Time: 2.63 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,28 +1,47 @@
 import java.util.*;
 
+class Node{
+    int value;
+    int idx;
+    
+    public Node(int value,int idx){
+        this.value = value;
+        this.idx = idx;
+    }
+}
+
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
         
-        PriorityQueue<Integer> p = new PriorityQueue<>(Collections.reverseOrder());
+        Queue<Node> q = new LinkedList<>();
         
-        for(int i : priorities){
-            p.offer(i);
+        for(int i = 0; i < priorities.length; i++){
+            q.offer(new Node(priorities[i],i));
         }
         
-        while(!p.isEmpty()){
-            for(int i = 0; i < priorities.length; i++){
-                if(priorities[i] == p.peek()){
-                    p.poll();
-                    answer++;
-                
-                    if(i == location){
-                        return answer;
-                    }
+        while(!q.isEmpty()){
+            Node now = q.poll();
+            
+            boolean check = false;
+            for(Node next : q){
+                if(next.value > now.value){
+                    check = true;
+                    break;
                 }
-            }    
+            }
+            
+            if(check){
+                q.offer(now);
+            }else{
+                answer++;
+                
+                if(now.idx == location){
+                    return answer;
+                }
+            }
         }
         
         return answer;
-    }
-}
+    }//solution end
+}//class end

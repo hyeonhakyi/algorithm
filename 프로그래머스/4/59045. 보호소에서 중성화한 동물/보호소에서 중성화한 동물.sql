83861616c5fc66d13ef7b9c30a3b753e3cb142ec
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-05-15 15:49:32 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -5,9 +5,9 @@ select
 from
     ANIMAL_INS as i
 join
-    ANIMAL_OUTS as o on i.ANIMAL_ID = o.ANIMAL_ID
+    ANIMAL_OUTS as o on i.ANIMAL_ID = o.ANIMAL_ID 
 where
-    i.SEX_UPON_INTAKE like 'Intact%'
-    and o.SEX_UPON_OUTCOME not like 'Intact%'
+    i.SEX_UPON_INTAKE like '%Intact%'
+    and o.SEX_UPON_OUTCOME not like '%Intact%'
 order by
     i.ANIMAL_ID

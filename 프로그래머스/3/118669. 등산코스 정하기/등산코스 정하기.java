c156ef188dbabaa9fad1c434b7bab1364064cf08
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-05-18 16:26:17 +0900

    [level 3] Title: 등산코스 정하기, Time: 111.13 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -1,89 +1,111 @@
 import java.util.*;
 
-class Node implements Comparable<Node>{
-    int end;
-    int weight;
-    
-    public Node(int end,int weight){
-        this.end = end;
-        this.weight = weight;
-    }
-    
-    @Override
-    public int compareTo(Node o){
-        return Integer.compare(this.weight,o.weight);
-    }
-}
-
 class Solution {
+    static class Edge{
+        int idx;
+        int weight;
+        public Edge(int idx,int weight){
+            this.idx = idx;
+            this.weight = weight;
+        }
+    }
+    
+    static class Node implements Comparable<Node>{
+        int idx;
+        int intensity;
+        public Node(int idx,int intensity){
+            this.idx = idx;
+            this.intensity = intensity;
+        }
+        
+        @Override
+        public int compareTo(Node o){
+            return Integer.compare(this.intensity,o.intensity);
+        }
+    }
+    static List<Edge>[] list;
+    static boolean[] isGate;
+    static boolean[] isSummits;
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
-        ArrayList<Node>[] list = new ArrayList[n+1];
-        boolean[] isGate = new boolean[n+1];
-        boolean[] isSummit = new boolean[n+1];
+        int[] answer = new int[2];
         
-        for(int g : gates){
-            isGate[g] = true;
-        }
-        
-        for(int s : summits){
-            isSummit[s] = true;
-        }
+        list = new ArrayList[n + 1];
+        isGate = new boolean[n + 1];
+        isSummits = new boolean[n + 1];
         
         for(int i = 1; i <= n; i++){
             list[i] = new ArrayList<>();
         }
         
-        for(int[] i : paths){
-            int start = i[0];
-            int end = i[1];
-            int weight = i[2];
-            
-            list[start].add(new Node(end,weight));
-            list[end].add(new Node(start,weight));
+        for(int gate : gates){
+            isGate[gate] = true;
         }
         
-        int INF = Integer.MAX_VALUE;
-        int[] dist = new int[n+1];
-        Arrays.fill(dist,INF);
+        for(int summit : summits){
+            isSummits[summit] = true;
+        }
         
+        for(int[] path : paths){
+            int s = path[0];
+            int e = path[1];
+            int w = path[2];
+            
+            list[s].add(new Edge(e,w));
+            list[e].add(new Edge(s,w));
+        }
+
+        int[] dist = djkstra(n,gates);
+        
+        int minSummit = Integer.MAX_VALUE;
+        int minIntensity = Integer.MAX_VALUE;
+        
+        Arrays.sort(summits);
+        
+        for(int summit : summits){
+            if(dist[summit] < minIntensity){
+                minIntensity = dist[summit];
+                minSummit = summit;
+            }
+        }
+        
+        return new int[] {minSummit,minIntensity};
+    }//soliution end
+    
+    private static int[] djkstra(int n,int[] gates){
         PriorityQueue<Node> q = new PriorityQueue<>();
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist,Integer.MAX_VALUE);
         
-        for(int g : gates){
-            dist[g] = 0;
-            q.offer(new Node(g,0));
+        for(int gate : gates){
+            dist[gate] = 0;
+            q.offer(new Node(gate,0));
         }
         
         while(!q.isEmpty()){
             Node now = q.poll();
-            int nowI = now.end;
-            int nowWeight = now.weight;
             
-            if(nowWeight > dist[nowI]) continue;
-            if(isSummit[nowI]) continue;
+            if(now.intensity > dist[now.idx]){
+                continue;
+            }
             
-            for(Node next : list[nowI]){
-                int end = next.end;
-                int weight = next.weight;
-                if(isGate[end]) continue;
+            if(isSummits[now.idx]){
+                continue;
+            }
+            
+            for(Edge next : list[now.idx]){
+                if(isGate[next.idx]){
+                    continue;
+                }
                 
-                int nextWeight = Math.max(nowWeight,weight);
-                if(dist[end] > nextWeight){
-                    dist[end] = nextWeight;
-                    q.offer(new Node(end,nextWeight));
+                int weight = Math.max(next.weight,now.intensity);
+                
+                if(weight < dist[next.idx]){
+                    q.offer(new Node(next.idx,weight));
+                    dist[next.idx] = weight;
                 }
             }
         }
         
-        Arrays.sort(summits);
-        int bestSummit = -1;
-        int bestWeight = INF;
-        for(int s : summits){
-            if(dist[s] < bestWeight){
-                bestWeight = dist[s];
-                bestSummit = s;
-            }
-        }
-        
-        return new int[]{bestSummit,bestWeight};
-    }
-}
+        return dist;
+    }//djkstra end
+}//class end

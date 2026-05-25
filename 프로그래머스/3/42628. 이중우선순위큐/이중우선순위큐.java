75cebb0b6c578aea04288eb812d1c390beb5f55e
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-05-25 14:52:46 +0900

    [level 3] Title: 이중우선순위큐, Time: 39.06 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,30 +1,27 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String[] operations) {
-        
+    public int[] solution(String[] operations) {        
         TreeMap<Integer,Integer> map = new TreeMap<>();
         
-        for(String str : operations){
-            String[] arr = str.split(" ");
-            String s = arr[0];
-            int num = Integer.parseInt(arr[1]);
-            
-            if(s.equals("I")){
+        for(int i = 0; i < operations.length; i++){
+            String[] str = operations[i].split(" ");
+            int num = Integer.parseInt(str[1]);
+            if(str[0].equals("I")){
                 map.put(num,map.getOrDefault(num,0) + 1);
             }else{
                 if(map.isEmpty()) continue;
                 
-                if(num == 1){
-                    int key = map.lastKey();
+                if(num == -1){
+                    int key = map.firstKey();
                     int cnt = map.get(key);
-                    if(cnt == 1) {
+                    if(cnt == 1){
                         map.remove(key);
                     }else{
-                        map.put(key,cnt-1);
+                        map.put(key,cnt - 1);
                     }
                 }else{
-                    int key = map.firstKey();
+                    int key = map.lastKey();
                     int cnt = map.get(key);
                     if(cnt == 1){
                         map.remove(key);
@@ -34,10 +31,11 @@ class Solution {
                 }
             }
         }
+        
         if(map.isEmpty()){
             return new int[]{0,0};
+        }else{
+            return new int[]{map.lastKey(),map.firstKey()};
         }
-            
-        return new int[]{map.lastKey(),map.firstKey()};
     }//solution end
 }//class end

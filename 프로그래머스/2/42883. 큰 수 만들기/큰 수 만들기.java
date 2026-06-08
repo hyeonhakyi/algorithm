99f23d6aa6ce1b813f7608777146518212a5cc47
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-06-08 15:23:36 +0900

    [level 2] Title: 큰 수 만들기, Time: 21.80 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -2,22 +2,27 @@ import java.util.*;
 
 class Solution {
     public String solution(String number, int k) {
-        StringBuilder answer = new StringBuilder();
-        char[] arr = number.toCharArray();
-        int len = number.length() - k;
-        
-        int start = 0;
-        for(int i = 0; i < len; i++){
-            char max = '0';
-            for(int j = start; j <= i + k; j++){
-                if(arr[j] > max){
-                    max = arr[j];
-                    start = j + 1;
-                }
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < number.length(); i++) {
+            char now = number.charAt(i);
+
+            // 수정: 제거할 수 있고, 앞 숫자가 현재 숫자보다 작으면 제거
+            while (k > 0 && sb.length() > 0 && sb.charAt(sb.length() - 1) < now) {
+                sb.deleteCharAt(sb.length() - 1);
+                k--;
             }
-            answer.append(max);
+
+            // 수정: 현재 숫자를 뒤에 추가
+            sb.append(now);
         }
-        
-        return answer.toString();
-    }
-}
+
+        // 수정: 아직 제거해야 할 숫자가 남았다면 뒤에서 제거
+        while (k > 0) {
+            sb.deleteCharAt(sb.length() - 1);
+            k--;
+        }
+
+        return sb.toString();
+    }//solution end
+}//class end

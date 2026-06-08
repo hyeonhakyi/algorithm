b152b48fead9c4f0f3301a4ad089a63c48266fa0
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-06-08 15:08:54 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 7.15 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,56 +1,51 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
     public int solution(String str1, String str2) {
+        Map<String, Integer> map1 = makeMap(str1);
+        Map<String, Integer> map2 = makeMap(str2);
         
-        str1 = str1.toLowerCase();
-		str2 = str2.toLowerCase();
-		
-		ArrayList<String> list1 = new ArrayList<>();
-		ArrayList<String> list2 = new ArrayList<>();
-		
-		ArrayList<String> union = new ArrayList<>();
-		ArrayList<String> unionAll = new ArrayList<>();		
-		
-		for(int i = 0; i < str1.length()-1; i++) {
-			char stt = str1.charAt(i);
-			char stt1 = str1.charAt(i+1);
-			
-			if((stt >= 'a' && stt <= 'z')&&(stt1 >= 'a' && stt1 <= 'z')) {
-				list1.add(String.valueOf(stt) + String.valueOf(stt1));
-			}	
-		}
-		
-		for(int i = 0; i < str2.length()-1; i++) {
-			char stt = str2.charAt(i);
-			char stt1 = str2.charAt(i+1);
-			
-			if((stt >= 'a' && stt <= 'z')&&(stt1 >= 'a' && stt1 <= 'z')) {
-				list2.add(String.valueOf(stt) + String.valueOf(stt1));
-			}	
-		}
-		
-		
-		for(String s : list1) {
-			if(list2.remove(s)) {
-				union.add(s);
-			}
-			unionAll.add(s);
-		}
+        int intersection = 0;
+        int union = 0;
         
-        unionAll.addAll(list2);
+        Set<String> keySet = new HashSet<>();
         
+        keySet.addAll(map1.keySet());
+        keySet.addAll(map2.keySet());
         
-        if(unionAll.size() == 0){
+        for(String key : keySet){
+            int count1 = map1.getOrDefault(key, 0);
+            int count2 = map2.getOrDefault(key, 0);
+            
+            intersection += Math.min(count1,count2);
+            union += Math.max(count1,count2);
+        }
+        
+        if(union == 0){
             return 65536;
         }
         
+        return (int) ((double) intersection / union * 65536);
+    }//solution end
+    
+    private static Map<String, Integer> makeMap(String str){
+        Map<String,Integer> map = new HashMap<>();
         
-		
-		double temp = (double)(union.size())/(double)(unionAll.size());
-        temp = temp * 65536;
+        str = str.toUpperCase();
         
-        int answer = (int)temp;
-		return answer;   
-    }
-}
+        for(int i = 0; i < str.length() - 1; i++){
+            char first = str.charAt(i);
+            char second = str.charAt(i + 1);
+            
+            if(check(first) && check(second)){
+                String word = "" + first + second;
+                map.put(word,map.getOrDefault(word,0) + 1);
+            }
+        }
+        return map;
+    }//makeMap end
+    
+    private static boolean check(char c){
+        return c >= 'A' && c <= 'Z';
+    }//check end
+}//class end

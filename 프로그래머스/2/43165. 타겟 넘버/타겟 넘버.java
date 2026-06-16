80f466d742871a448b006537778f614f263cd365
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-06-16 15:18:59 +0900

    [level 2] Title: 타겟 넘버, Time: 3.37 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,23 +1,27 @@
 import java.util.*;
 
 class Solution {
-    static int answer;
+    static int n,answer;
+    static boolean[] visited;
     public int solution(int[] numbers, int target) {
         answer = 0;
+        n = numbers.length;
+        visited = new boolean[n];
+        
         dfs(numbers,target,0,0);
         
         return answer;
     }//solution end
     
-    private static void dfs(int[] numbers,int target,int sum,int idx){
-        if(idx == numbers.length){
+    private static void dfs(int[] numbers,int target,int idx,int sum){
+        if(idx == n){
             if(sum == target){
                 answer++;
             }
             return;
         }
         
-        dfs(numbers,target,sum + numbers[idx],idx + 1);
-        dfs(numbers,target,sum - numbers[idx],idx + 1);
+        dfs(numbers,target,idx + 1,sum + numbers[idx]);
+        dfs(numbers,target,idx + 1,sum - numbers[idx]);
     }//dfs end
 }//class end

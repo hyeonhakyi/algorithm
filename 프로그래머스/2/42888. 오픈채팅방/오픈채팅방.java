38a000d36f8847439a32cf89cb03deb6230ac92f
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-07-02 14:30:12 +0900

    [level 2] Title: 오픈채팅방, Time: 71.50 ms, Memory: 151 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -2,37 +2,37 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] record) {
-        Map<String , String> map = new HashMap<>();
-        int count = 0;
+        HashMap<String,String> map = new HashMap<>();
+        List<String[]> list = new ArrayList<>();
         
         for(int i = 0; i < record.length; i++){
-            String[] info = record[i].split(" ");
+            String[] arr = record[i].split(" ");
             
-            if(info[0].equals("Leave")){
-                continue;
-            } else if(info[0].equals("Enter")){
-                map.put(info[1],info[2]);
+            String command = arr[0];
+            String userId = arr[1];
+            
+            if(command.equals("Enter")){
+                map.put(userId,arr[2]);
+                list.add(new String[]{userId,"Enter"});
+            }else if(command.equals("Leave")){
+                list.add(new String[]{userId,"Leave"});
+            }else if(command.equals("Change")){
+                map.put(userId,arr[2]);
+            }
+        }
+        String[] answer = new String[list.size()];
+        
+        for(int i = 0; i < list.size(); i++){
+            String userId = list.get(i)[0];
+            String command = list.get(i)[1];
+            
+            if(command.equals("Enter")){
+                answer[i] = map.get(userId) + "님이 들어왔습니다.";
             }else{
-                map.put(info[1],info[2]);
-                count++;
+                answer[i] = map.get(userId) + "님이 나갔습니다.";
             }
         }
-        
-        String[] answer = new String[record.length - count];
-        
-        int idx = 0;
-        for(int i = 0; i < record.length; i++){
-            String[] info = record[i].split(" ");
-            String nickname = map.get(info[1]);
-            
-            if(info[0].equals("Enter")){
-                answer[idx++] = nickname + "님이 들어왔습니다.";
-            }else if (info[0].equals("Leave")){
-                answer[idx++] = nickname + "님이 나갔습니다.";
-            }
-        }
-        
         
         return answer;
-    }
-}
+    }//solution end
+}//class end

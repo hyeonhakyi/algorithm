afc20cccec49ba010b6be60b041c225e39a34d50
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-07-08 13:15:59 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,35 +2,36 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> result = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         
         int n = progresses.length;
-        int currentDay = getDay(progresses[0],speeds[0]);
+        int currentDay = cal(progresses[0],speeds[0]);
         int count = 1;
         
         for(int i = 1; i < n; i++){
-            int day = getDay(progresses[i],speeds[i]);
+            int day = cal(progresses[i],speeds[i]);
             
             if(day <= currentDay){
                 count++;
             }else{
-                result.add(count);
+                list.add(count);
                 currentDay = day;
                 count = 1;
             }
         }
         
-        result.add(count);
+        list.add(count);
         
-        int[] answer = new int[result.size()];
-        for(int i = 0; i < result.size(); i++){
-            answer[i] = result.get(i);
+        int[] answer = new int[list.size()];
+        
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
         }
         
         return answer;
     }//solution end
     
-    private static int getDay(int progresse,int speed){
+    private static int cal(int progresse,int speed){
         return (100 - progresse + speed - 1) / speed;
-    }//getDay end
+    }//cal end
 }//class end

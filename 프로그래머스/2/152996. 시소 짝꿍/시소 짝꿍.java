94171c04301b10827a793c91b8dfa40b8f762d06
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-07-09 14:54:48 +0900

    [level 2] Title: 시소 짝꿍, Time: 4116.92 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -6,22 +6,23 @@ class Solution {
         
         Arrays.sort(weights);
         
-        Map<Double,Integer> map = new HashMap<>();
-        
-        for(int w : weights){
-            double a = w * 1.0;
-            double b = (w * 2.0) / 3.0;
-            double c = (w * 2.0) / 4.0;
-            double d = (w * 3.0) / 4.0;
-            
-            if(map.containsKey(a)) answer += map.get(a);
-            if(map.containsKey(b)) answer += map.get(b);
-            if(map.containsKey(c)) answer += map.get(c);
-            if(map.containsKey(d)) answer += map.get(d);
-            
-            map.put((w * 1.0), map.getOrDefault((w * 1.0),0) + 1);
+        for(int i = 0; i < weights.length; i++){
+            int first = weights[i];
+            for(int j = i + 1; j < weights.length; j++){
+                int second = weights[j];
+                
+                if(first == second){
+                    answer++;
+                }else if((first * 4) == (second * 2)){
+                    answer++;
+                }else if((first * 3) == (second * 2)){
+                    answer++;
+                }else if((first * 4) == (second * 3)){
+                    answer++;
+                }
+            }
         }
         
         return answer;
-    }
-}
+    }//solution end
+}//class end

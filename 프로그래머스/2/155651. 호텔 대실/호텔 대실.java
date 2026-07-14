a2ed2b55dc5af757da9afe13509677a5136f60c1
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-07-14 14:56:56 +0900

    [level 2] Title: 호텔 대실, Time: 6.48 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,31 +1,51 @@
 import java.util.*;
 
-class Solution {
-    public int solution(String[][] book_time) {
-        int answer = 0;
-        int[] time = new int[24 * 60 + 10];
-        
-        for(String[] room : book_time){
-            String start = room[0];
-            String end = room[1];
-            
-            time[cal(start)] += 1;
-            time[cal(end) + 10] -= 1;
-        }
-        
-        for(int i = 1; i < time.length; i++){
-            time[i] += time[i - 1];
-            answer = Math.max(answer,time[i]);
-        }
-        
-        return answer;
-    }
-    
-    public static int cal(String time){
-        String[] t = time.split(":");
-        int h = Integer.parseInt(t[0]);
-        int m = Integer.parseInt(t[1]);
-        
-        return (h * 60) + m;
+class Node{
+    int start;
+    int end;
+    public Node(int start,int end){
+        this.start = start;
+        this.end = end;
     }
 }
+
+class Solution {
+    public int solution(String[][] book_time) {
+        ArrayList<Node> list = new ArrayList<>();
+        
+        for(String[] arr : book_time){
+            int start = change(arr[0]);
+            int end = change(arr[1]) + 10;
+            
+            list.add(new Node(start,end));
+        }
+        
+        list.sort((a,b) -> {
+            if(a.start == b.start){
+                return Integer.compare(a.end,b.end);
+            }
+            return Integer.compare(a.start,b.start);
+        });
+        
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        
+        for(Node next : list){
+            if(!q.isEmpty() && q.peek() <= next.start){
+                q.poll();
+            }
+            
+            q.offer(next.end);
+        }
+        
+        return q.size();
+    }//solution end
+    
+    private static int change(String time){
+        String[] arr = time.split(":");
+        
+        int hour = Integer.parseInt(arr[0]) * 60;
+        int minute = Integer.parseInt(arr[1]);
+        
+        return hour + minute;
+    }//change
+}//class end

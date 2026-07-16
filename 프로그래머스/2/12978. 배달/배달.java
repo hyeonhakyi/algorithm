ec4a212d37b59915a76246552cd7a45a92b1c36b
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-07-16 14:08:43 +0900

    [level 2] Title: 배달, Time: 2.39 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,48 +1,41 @@
 import java.util.*;
 
-class Node implements Comparable<Node> {
+class Node{
     int idx;
     int weight;
-    
     public Node(int idx,int weight){
         this.idx = idx;
         this.weight = weight;
     }
-    
-    @Override
-    public int compareTo(Node o){
-        return this.weight - o.weight;
-    }
 }
 
 class Solution {
-    static ArrayList<Node>[] list;
-    static int k,n;
+    static List<Node>[] list;
     static int[] dist;
+    static int answer;
     public int solution(int N, int[][] road, int K) {
-        int answer = 0;
-        k = K;
-        n = N;
+        answer = 0;
+        
         list = new ArrayList[N + 1];
         
         for(int i = 1; i <= N; i++){
             list[i] = new ArrayList<>();
         }
         
+        for(int[] i : road){
+            int s = i[0];
+            int e = i[1];
+            int w = i[2];
+            
+            list[s].add(new Node(e,w));
+            list[e].add(new Node(s,w));
+        }
+        
         dist = new int[N + 1];
         Arrays.fill(dist,Integer.MAX_VALUE);
         dist[1] = 0;
         
-        for(int[] r : road){
-            int start = r[0];
-            int end = r[1];
-            int weight = r[2];
-            
-            list[start].add(new Node(end,weight));
-            list[end].add(new Node(start,weight));
-        }
-        
-        dijkstra();
+        bfs(K);
         
         for(int i = 1; i <= N; i++){
             if(dist[i] <= K){
@@ -51,18 +44,14 @@ class Solution {
         }
         
         return answer;
-    }//main end
+    }//solution end
     
-    public static void dijkstra(){
-        PriorityQueue<Node> q = new PriorityQueue<>();
+    private static void bfs(int K){
+        Queue<Node> q = new LinkedList<>();
         q.offer(new Node(1,0));
-        boolean[] visited = new boolean[n + 1];
         
         while(!q.isEmpty()){
             Node now = q.poll();
-            
-            if(visited[now.idx]) continue;
-            visited[now.idx] = true;
             
             for(Node next : list[now.idx]){
                 if(dist[next.idx] > dist[now.idx] + next.weight){
@@ -71,5 +60,6 @@ class Solution {
                 }
             }
         }
-    }//dijkstra end
-}
+        return;
+    }//bfs end
+}//class end

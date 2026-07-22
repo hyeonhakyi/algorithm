f3bd13e38033ce740f800e69c7f4bed235f0bf39
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-07-22 14:57:55 +0900

    [level 1] Title: K번째수, Time: 1.90 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,13 +4,24 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
+        int num = 0;
         for(int i = 0; i < commands.length; i++){
-            int[] temp = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
+            int s = commands[i][0] - 1;
+            int e = commands[i][1] - 1;
+            int k = commands[i][2] - 1;
             
-            Arrays.sort(temp);
-            answer[i] = temp[commands[i][2] - 1];
+            int[] arr = new int[e - s + 1];
+            int idx = 0;
+            
+            for(int j = s; j <= e; j++){
+                arr[idx++] = array[j];
+            }
+            
+            Arrays.sort(arr);
+            
+            answer[num++] = arr[k];
         }
         
         return answer;
-    }
-}
+    }//solution end
+}//class end
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
 
 ### 성능 요약
 
-메모리: 65.5 MB, 시간: 0.53 ms
+메모리: 81.5 MB, 시간: 1.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 13일 16:04:36
+2026년 07월 22일 14:58:04
 
 ### 문제 설명
 

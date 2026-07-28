15de98a9a391bf8a5fea7087d82807f36d735689
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-07-28 15:16:55 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.05 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -3,26 +3,38 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, long k) {
         int[] answer = new int[n];
-        
-        long[] fact = new long[n + 1];
-        fact[0] = 1;
-        for(int i = 1; i <= n; i++){
-            fact[i] = fact[i - 1] * i;
+
+        // factorial[i] = i!
+        long[] factorial = new long[n + 1];
+        factorial[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            factorial[i] = factorial[i - 1] * i;
         }
-        
-        List<Integer> list = new ArrayList<>();
-        for(int i = 1; i <= n; i++){
-            list.add(i);
+
+        // 아직 선택하지 않은 숫자
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            numbers.add(i);
         }
-        
+
+        // 1부터 시작하는 순서를 0부터 시작하도록 변경
         k--;
-        for(int i = 0; i < n; i++){
-            long block = fact[n - 1 - i];
-            int idx = (int)(k / block);
-            answer[i] = list.remove(idx);
-            k %= block;
+
+        for (int position = 0; position < n; position++) {
+            // 현재 숫자를 하나 선택한 후 만들 수 있는 순열 개수
+            long blockSize = factorial[n - 1 - position];
+
+            // 몇 번째 숫자를 선택할지 계산
+            int index = (int) (k / blockSize);
+
+            answer[position] = numbers.remove(index);
+
+            // 선택한 묶음 내부에서의 순서
+            k %= blockSize;
         }
-        
+
         return answer;
-    }
-}
+    }//solution end
+}//class end

d50cae48b8451fba29d3457209daadf1f766b43d
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-08-25 16:58:54 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -3,19 +3,19 @@ import java.util.*;
 class Solution {
     public int solution(String name) {
         int answer = 0;
-        int len = name.length();
         
+        int len = name.length();
         int move = len - 1;
         
         for(int i = 0; i < len; i++){
-            char c = name.charAt(i);
+            char num = name.charAt(i);
             
-            int up = c - 'A';
-            int down = 'Z' - c + 1;
+            int up = num - 'A';
+            int down = 'Z' - num + 1;
+            
             answer += Math.min(up,down);
             
             int next = i + 1;
-            
             while(next < len && name.charAt(next) == 'A'){
                 next++;
             }

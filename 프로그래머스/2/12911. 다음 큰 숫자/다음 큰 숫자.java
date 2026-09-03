022b510128627ba4c5ef6ca40562bc10d3196313
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-09-03 15:00:31 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(int n) {
+        int answer = 0;
         int target = n;
         String str = Integer.toBinaryString(n);
         int totalCnt = 0;
@@ -11,29 +12,22 @@ class Solution {
             }
         }
         
-        
         while(true){
             target++;
-            if(find(target,totalCnt)){
+            
+            String tar = Integer.toBinaryString(target);
+            int targetCnt = 0;
+            for(int i = 0; i < tar.length(); i++){
+                if(tar.charAt(i) == '1'){
+                    targetCnt++;
+                }
+            }
+            
+            if(targetCnt == totalCnt){
                 break;
             }
         }
         
         return target;
     }//solution end
-    
-    private static boolean find(int target,int total){
-        String str = Integer.toBinaryString(target);
-        int cnt = 0;
-        for(int i = 0; i < str.length(); i++){
-            if(str.charAt(i) == '1'){
-                cnt++;
-            }
-        }
-        
-        if(cnt == total){
-            return true;
-        }
-        return false;
-    }//find end
-}//clcass end
+}//class end

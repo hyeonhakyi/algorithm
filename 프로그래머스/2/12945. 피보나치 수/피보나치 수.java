03e19348b49c0fdeda0f7e8f2078b9ff57560461
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-09-07 14:46:27 +0900

    [level 2] Title: 피보나치 수, Time: 3.60 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -3,15 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int[] dp = new int[n + 1];
+        int[] dp = new int[100001];
         
         dp[0] = 0;
         dp[1] = 1;
+        dp[2] = 1;
         
-        for(int i = 2; i <= n; i++){
+        for(int i = 3; i < 100001; i++){
             dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
         }
         
-        return dp[n];
+        return dp[n] % 1234567;
     }//solution end
 }//class end

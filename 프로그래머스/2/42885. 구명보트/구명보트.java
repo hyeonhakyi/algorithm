59b0e787a544cb61cd46b5f9994c3a5e0b624e79
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-09-09 14:36:34 +0900

    [level 2] Title: 구명보트, Time: 8.85 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -4,16 +4,16 @@ class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         
-        Arrays.sort(people);
         int left = 0;
         int right = people.length - 1;
+        Arrays.sort(people);
         
         while(left <= right){
             int sum = people[left] + people[right];
             
             if(sum > limit){
-                right--;
                 answer++;
+                right--;
             }else{
                 left++;
                 right--;

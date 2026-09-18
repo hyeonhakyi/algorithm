37cee9beb45dacc94b4b7f51b261065f2cf7a60a
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-09-18 15:30:03 +0900

    [level 3] Title: 아이템 줍기, Time: 3.08 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -3,64 +3,62 @@ import java.util.*;
 class Node{
     int x;
     int y;
-    int cnt;
-    public Node(int x,int y,int cnt){
+    int dist;
+    public Node(int x,int y,int dist){
         this.x = x;
         this.y = y;
-        this.cnt = cnt;
+        this.dist = dist;
     }
 }
 
 class Solution {
-    static int [][] arr = new int[102][102];
+    static int[][] arr = new int[102][102];
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         int answer = 0;
         
-        for(int[] r : rectangle){
-            int x1 = r[0] * 2;
-            int y1 = r[1] * 2;
-            int x2 = r[2] * 2;
-            int y2 = r[3] * 2;
+        for(int[] rec : rectangle){
+            int x = rec[0] * 2;
+            int y = rec[1] * 2;
+            int x1 = rec[2] * 2;
+            int y1 = rec[3] * 2;
             
-            for(int i = x1; i <= x2 ; i++){
-                for(int j = y1; j <= y2; j++){
+            for(int i = x; i <= x1; i++){
+                for(int j = y; j <= y1; j++){
                     arr[i][j] = 1;
                 }
             }
         }
         
-        for(int[] r : rectangle){
-            int x1 = r[0] * 2;
-            int y1 = r[1] * 2;
-            int x2 = r[2] * 2;
-            int y2 = r[3] * 2;
+        for(int[] rec : rectangle){
+            int x = rec[0] * 2;
+            int y = rec[1] * 2;
+            int x1 = rec[2] * 2;
+            int y1 = rec[3] * 2;
             
-            for(int i = x1 + 1; i < x2; i++){
-                for(int j = y1 + 1; j < y2; j++){
+            for(int i = x + 1; i < x1; i++){
+                for(int j = y + 1; j < y1; j++){
                     arr[i][j] = 0;
                 }
             }
         }
         
-        answer = bfs(characterX * 2, characterY * 2, itemX * 2, itemY * 2);
         
-        
-        return answer / 2;
+        return bfs(characterX * 2, characterY * 2, itemX * 2, itemY * 2) / 2;
     }//solution end
     
     private static int bfs(int sx,int sy,int ex,int ey){
         Queue<Node> q = new LinkedList<>();
         boolean[][] visited = new boolean[102][102];
-        q.offer(new Node(sx,sy,1));
+        q.offer(new Node(sx,sy,0));
         visited[sx][sy] = true;
         
         while(!q.isEmpty()){
             Node now = q.poll();
             
             if(now.x == ex && now.y == ey){
-                return now.cnt;
+                return now.dist + 1;
             }
             
             for(int d = 0; d < 4; d++){
@@ -70,12 +68,12 @@ class Solution {
                 if(!check(nx,ny)) continue;
                 if(visited[nx][ny]) continue;
                 if(arr[nx][ny] == 0) continue;
-                q.offer(new Node(nx,ny,now.cnt + 1));
+                
                 visited[nx][ny] = true;
+                q.offer(new Node(nx,ny,now.dist + 1));
             }
         }
-        
-        return - 1;
+        return 0;
     }//bfs end
     
     private static boolean check(int x,int y){

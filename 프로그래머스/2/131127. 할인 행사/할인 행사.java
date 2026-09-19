c46572d6f68aa603b9dc42b1e43afde73d3e8db3
이학현 <108259054+hyeonhakyi@users.noreply.github.com>
2026-09-19 13:49:19 +0900

    [level 2] Title: 할인 행사, Time: 32.13 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,33 +1,33 @@
 import java.util.*;
 
 class Solution {
-    static int answer;
     public int solution(String[] want, int[] number, String[] discount) {
-        answer = 0;
+        int answer = 0;
         
-        int totalDay = 0;
+        int today = 0;
         for(int i : number){
-            totalDay += i;
+            today += i;
         }
         
-        for(int i = 0; i <= discount.length - totalDay; i++){
-            Map<String,Integer> map = new HashMap<>();
+        for(int i = 0; i <= discount.length - today; i++){
+            HashMap<String,Integer> map = new HashMap<>();
             
-            for(int j = i; j < i + totalDay; j++){
+            for(int j = i; j < i + today; j++){
                 map.put(discount[j],map.getOrDefault(discount[j],0) + 1);
             }
+            
             if(check(map,want,number)){
-                        answer++;
+                answer++;
             }
         }
-        
+
         return answer;
     }//solution end
     
-    private static boolean check(Map<String,Integer> map,String[] want,int[] number){
+    private static boolean check(HashMap<String,Integer> map,String[] want,int[] number){
         for(int i = 0; i < want.length; i++){
-            if(map.getOrDefault(want[i], 0) != number[i]){
-                return false;
+           if(map.getOrDefault(want[i],0) != number[i]){
+              return false; 
             }
         }
         return true;
